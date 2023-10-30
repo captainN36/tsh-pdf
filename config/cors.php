99da7ef694d8918b208e976d7676e173792259d2
Huy Tran H. VN.Danang <108646730+mvn-huytran-dn@users.noreply.github.com>
2023-10-31 01:02:02 +0700

    fix: allow cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://xem.tracuuthansohoconline.com'],
+    'allowed_origins' => ['https://xem.tracuuthansohoconline.com', 'http://localhost:3001'],
 
     'allowed_origins_patterns' => [],
 

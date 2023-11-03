1a25d86d539188e0124fda07f36b9cd20f7ef9c8
Huy Tran H. VN.Danang <108646730+mvn-huytran-dn@users.noreply.github.com>
2023-11-03 16:37:04 +0700

    add code debug

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
-    <meta charset="utf-8"/>
-    <meta name="generator" content="pdf2htmlEX"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <meta charset="utf-8" />
+    <meta name="generator" content="pdf2htmlEX" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <link rel="stylesheet" href="{{ asset('/pdf.css') }}">
     <title>test</title>
 </head>
@@ -12,26 +13,29 @@
     </div>
 </div>
 <div id="page-container">
-    @include('pages.first')
-    @include('pages.chiso')
-    @include('pages.vanso')
-    @include('pages.duongdoi')
+    {{-- @include('pages.first') --}}
+    {{-- @include('pages.chiso') --}}
+    {{-- @include('pages.vanso') --}}
+    {{-- @include('pages.duongdoi') --}}
 
 
     <div id="pf7" class="pf w0 h0" data-page-no="54">
         <div class="pc pc6 w0 h0 opened">
-            <img class="bi x0 y0 w1 h1" alt="" src="{{ asset('/'.$data['data']['lifePathIndicator']['lifePathIndicator'].'/vochong.png') }}">
+            <img class="bi x0 y0 w1 h1" alt=""
+                src="{{ asset('/' . $data['data']['lifePathIndicator']['lifePathIndicator'] . '/vochong.png') }}">
             <div class="t m0 x13 hd y93 ff1 fs7 fc0 sc0 ls0 ws0"></div>
             <div class="t m0 x14 he y94 ff1 fs8 fc0 sc0 ls0 ws0">TỔNG QUAN DỮ LIỆU BÁO CÁO</div>
             <div class="t m0 x5 hf ff2 fs9 fc2 sc0 ls0 ws0" style="left: 0px; top: 100px">
-                <img src="{{ asset('/'.$data['data']['lifePathIndicator']['lifePathIndicator']. '/report.png') }}" alt="" width="2000px">
+                <img src="{{ asset('/' . $data['data']['lifePathIndicator']['lifePathIndicator'] . '/report.png') }}"
+                    alt="" width="2000px">
             </div>
 
 
             <div class="t m2 xa h6 y5f ff3 fs2 fc0 sc0 ls0 ws0">Numerology Report</div>
             @include('footer')
             <div class="t m0 x3a h5 y61 ff2 fs2 fc0 sc0 ls0 ws0">55</div>
-            <div class="pi" data-data="{&quot;ctm&quot;:[1.500000,0.000000,0.000000,1.500000,0.000000,0.000000]}"></div>
+            <div class="pi" data-data="{&quot;ctm&quot;:[1.500000,0.000000,0.000000,1.500000,0.000000,0.000000]}">
+            </div>
         </div>
     </div>
 
@@ -49,4 +53,5 @@
 
 
 </body>
+
 </html>

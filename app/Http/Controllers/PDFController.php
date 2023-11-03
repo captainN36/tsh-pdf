22ca46565b5a0d1b55e33bf211c3033acce505f1
Huy Tran H. VN.Danang <108646730+mvn-huytran-dn@users.noreply.github.com>
2023-11-03 16:44:53 +0700

    add code debug

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -5,21 +5,25 @@ namespace App\Http\Controllers;
 use Barryvdh\DomPDF\Facade\Pdf;
 use ConvertApi\ConvertApi;
 use Illuminate\Http\Request;
+use Illuminate\Log\Logger;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Process;
 use Illuminate\Support\Facades\Storage;
 
 class PDFController extends Controller
 {
-    public function view(Request $request){
+    public function view(Request $request)
+    {
         $data = $this->getData($request->all());
         return view('welcome', ['data' => $data]);
     }
 
-    public function pdf(Request $request){
+    public function pdf(Request $request)
+    {
         $data = $this->getData($request->all());
         $name = $data['id'] . '-' . $data['dateSearch'] . '.html';
         $namePDF = $data['id'] . '-' . $data['dateSearch'] . '.pdf';
@@ -30,21 +34,24 @@ class PDFController extends Controller
             mkdir(public_path() . '/pdf/', 0777, true);
         }
         Process::run('chmod -R 777 ' . public_path());
-        $pathHtml = public_path() . '/html/'. $name;
-        $pathPDF = public_path() . '/pdf/'. $data['id'] . '-' . $data['dateSearch'] . '.pdf';
+        $pathHtml = public_path() . '/html/' . $name;
+        $pathPDF = public_path() . '/pdf/' . $data['id'] . '-' . $data['dateSearch'] . '.pdf';
         if (!file_exists($pathHtml) && !file_exists($pathPDF)) {
             $file = fopen($pathHtml, 'w+');
             fwrite($file, view('welcome', ['data' => $data])->render());
             try {
-                $result = Process::run('wkhtmltopdf ' . $pathHtml . ' ' . $pathPDF);
-            } catch (\Exception $exception){
-                return response()->json(['path' => asset('/pdf/'). $namePDF]);
+                $process = "wkhtmltopdf $pathHtml $pathPDF";
+                Log::info('process', ['process' => $process]);
+                Process::run($process);
+            } catch (\Exception $exception) {
+                throw $exception;
             }
         }
-        return response()->json(['path' => asset('/pdf/'). $namePDF]);
+        return response()->json(['path' => asset('/pdf/') . $namePDF]);
     }
 
-    public function renderPDF(Request $request){
+    public function renderPDF(Request $request)
+    {
         $data = $this->getData($request->all());
         $name = $data['id'] . '-' . $data['dateSearch'] . '.html';
         if (!Storage::exists($name)) {
@@ -56,7 +63,8 @@ class PDFController extends Controller
         return response()->json(['path' => asset($name)]);
     }
 
-    public function getData($data){
+    public function getData($data)
+    {
         $token = $data['token'];
         $url = $data['url'];
         $callAPI = Http::withHeaders([
@@ -65,15 +73,15 @@ class PDFController extends Controller
         $data = $callAPI->json();
         $title = $data['data']['data'];
         $count = 1;
-        foreach ($title as $key => $item){
-            if (isset($item['title'])){
-                if ($count <= 6){
+        foreach ($title as $key => $item) {
+            if (isset($item['title'])) {
+                if ($count <= 6) {
                     $data['data']['data'][$key]['page'] = 2;
                 }
-                if ($count > 6 && $count <= 13){
+                if ($count > 6 && $count <= 13) {
                     $data['data']['data'][$key]['page'] = 3;
                 }
-                if ($count > 13){
+                if ($count > 13) {
                     $data['data']['data'][$key]['page'] = 4;
                 }
             }

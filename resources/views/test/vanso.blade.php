9c06ab3b3d14f7c161acbca89e02092ec2eeaeb5
mor-nganct <caothuongngan@gmail.com>
2023-11-14 16:43:26 +0700

    commit

--- resources/views/test/vanso.blade.php
+++ resources/views/test/vanso.blade.php
@@ -14,7 +14,7 @@
     }
 </style>
 
-<div class="content-container" style="margin: 4em; margin-bottom: 4em">
+<div class="content-container" style="margin: 5em; margin-bottom: 5em; margin-top: 4em">
     {!! $data['data']['yearIndicator']['nextYearIndicator']['content'] !!}
     {!! $data['data']['yearIndicator']['nextYearIndicator']['content'] !!}
     {!! $data['data']['yearIndicator']['nextYearIndicator']['content'] !!}

875da58f31f9a1b36f506b80ad7b3287aae5c0e3
ngan_ct <caothuongngan@gmail.com>
2023-11-14 21:34:59 +0700

    commit

--- resources/views/files/vanso.blade.php
+++ resources/views/files/vanso.blade.php
@@ -201,7 +201,7 @@
             $nowMonthIndicator = \App\Http\Controllers\PDFController::renderText('nowMonthIndicator', $data['data']['monthIndicator']['nowMonthIndicator']['content'])
         ?>
         <div class="t m0 x5 h9 ff4 fs4 fc2 sc0 ls0 ws0" style="width: 2000px; white-space: normal; bottom: 800px; text-align: justify;">
-            {!! nl2br(e($nowMonthIndicator[$i])) !!}
+            {!! nl2br(e($nowMonthIndicator[1])) !!}
 
         </div>
 

50ed7e179989ced0e0cb8965dc247c8917075290
ngan_ct <caothuongngan@gmail.com>
2023-11-14 23:35:41 +0700

    commit

--- resources/views/test/vanso.blade.php
+++ resources/views/test/vanso.blade.php
@@ -14,7 +14,7 @@
     }
 </style>
 
-<div style="margin-bottom: 10em;">
+<div style="margin: 71px">
     <div>
         {!! "<br><br><br><br><br><br><br><br><br><br><br><br><br>" !!}
         {!! "<br><br><br><br><br><br><br><br><br><br><br><br><br>" !!}

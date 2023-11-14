3fd7d46831a4d25e9574ba66878ec316282a1f55
mor-nganct <caothuongngan@gmail.com>
2023-11-14 10:58:41 +0700

    test helper ..

--- resources/views/files/vanso.blade.php
+++ resources/views/files/vanso.blade.php
@@ -18,7 +18,7 @@
             {!! $data['data']['yearIndicator']['description'] !!}
         </div>
         <div class="t m0 x5 h9 yc7 ff4 fs4 fc2 sc0 ls0 ws0" style="width: 2000px; white-space: normal">
-            {!! \App\Http\Controllers\PDFController::renderText('chiso')[1] !!}
+            {!! \App\Http\Controllers\PDFController::renderText('content', $data['data']['yearIndicator']['nowYearIndicator']['content'])[1] !!}
         </div>
 
         <div class="t m2 xa h6 y5f ff3 fs2 fc0 sc0 ls0 ws0">Numerology Report</div>
@@ -34,7 +34,7 @@
         <img class="bi x0 y0 w1 h1" alt=""
             src="{{ asset('/' . $path . '/page-trang-trai.png') }}">
         <div class="t m0 x5 h12 yf3 ff3 fs4 fc2 sc0 ls0 ws0" style="width: 2000px; white-space: normal">
-            {!! \App\Http\Controllers\PDFController::renderText('chiso')[$i] !!}
+            {!! \App\Http\Controllers\PDFController::renderText('content', $data['data']['yearIndicator']['nowYearIndicator']['content'])[$i] !!}
         </div>
         <div class="t m2 xe h6 y5f ff3 fs2 fc0 sc0 ls0 ws0">Numerology Report</div>
         @include('footer', ['name' => $data['fullName'], 'date' => $data['dateOfBirth']])

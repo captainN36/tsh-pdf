4ca24bad1317407c91e13edb554ee419d01cf265
mor-nganct <caothuongngan@gmail.com>
2023-11-15 14:46:59 +0700

    commit

--- resources/views/files/duongdoi.blade.php
+++ resources/views/files/duongdoi.blade.php
@@ -32,9 +32,7 @@
         ?>
         <div class="t m0 x5 hf yd7 ff4 fs9 fc2 sc0 ls0 ws0" style="white-space: normal; width: 2000px">
             <div style="margin-top: 180px">
-                <?php dd($lifePathIndicator[1]) ?>
                 <?php $result = preg_replace('/\·\n\n/', '·', $lifePathIndicator[1]); ?>
-                <?php $result = str_replace("\n\n", "\n", $result); ?>
                 {!! nl2br(e($result)) !!}
             </div>
         </div>

e50a9fd8004c5a90b7a2fb29ec5622645a3d4e8f
mor-nganct <caothuongngan@gmail.com>
2023-11-15 14:06:48 +0700

    commit

--- resources/views/files/duongdoi.blade.php
+++ resources/views/files/duongdoi.blade.php
@@ -29,7 +29,6 @@
         </div>
         <?php
             $lifePathIndicator = \App\Http\Controllers\PDFController::renderText('lifePathIndicator', $data['data']['lifePathIndicator']['content']);
-            dd($lifePathIndicator);
         ?>
         <div class="t m0 x5 hf yd7 ff4 fs9 fc2 sc0 ls0 ws0" style="white-space: normal; width: 2000px">
             <div style="margin-top: 180px">

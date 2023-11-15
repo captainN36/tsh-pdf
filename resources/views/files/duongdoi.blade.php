475ce017396612f85299a313f924eec32f807b66
mor-nganct <caothuongngan@gmail.com>
2023-11-15 17:13:14 +0700

    commit

--- resources/views/files/duongdoi.blade.php
+++ resources/views/files/duongdoi.blade.php
@@ -140,7 +140,6 @@
             }
         ?>
         <div class="t m0 x5 hf yd7 ff2 fs9 fc2 sc0 ls0 ws0" style="white-space: normal;width: 2000px;bottom: 590px;">
-            <?php $first = str_replace("\n", '', $array[0]); ?>
             {!! nl2br(e($array[0])) !!}
         </div>
     </div>
@@ -158,8 +157,7 @@
             <img class="bi x0 y0 w1 h1" alt=""
                 src="{{ asset('/' . $path . '/page-trang-trai.png') }}">
             <div class="t m0 x5 h12 yf3 ff3 fs4 fc2 sc0 ls0 ws0" style="width: 2000px; white-space: normal">
-                <?php $content = str_replace("\n", '', $array[$i]); ?>
-                {!! nl2br(e($content)) !!}
+                {!! nl2br(e($array[$i])) !!}
             </div>
             <div class="t m2 xe h6 y5f ff3 fs2 fc0 sc0 ls0 ws0">Numerology Report</div>
             @include('footer', ['name' => $data['fullName'], 'date' => $data['dateOfBirth']])
@@ -322,11 +320,48 @@
             {!! $data['data']['attitudeIndicator']['description'] !!}
         </div>
         <?php
-            $attitudeIndicator = \App\Http\Controllers\PDFController::renderText('attitudeIndicator', $data['data']['attitudeIndicator']['content']);
+            $attitudeIndicator = \App\Http\Controllers\PDFController::renderText('attitudeIndicator', $data['data']['attitudeIndicator']['content'], false);
+            $inputString = '';
+            for ($i = 1; $i <= count($attitudeIndicator); $i++) {
+                $inputString .= $attitudeIndicator[$i];
+            }
+            $lines = explode("\n", $inputString);
+
+            $linesPerPartFirst = 30;
+            $linesPerPartRest = 100;
+
+            $parts = [];
+
+            for ($i = 0; $i < count($lines); $i += $linesPerPart) {
+                $linesPerPart = ($i == 0) ? $linesPerPartFirst : $linesPerPartRest;
+
+                $part = array_slice($lines, $i, $linesPerPart);
+                
+                $part = array_filter($part);
+
+                if (!empty($part)) {
+                    $parts[] = implode("\n", $part);
+                }
+            }
+            $array = [];
+            for ($i = 0; $i < count($parts); $i++) {
+                          
+                if ($i != 0) {
+                    $html = str_replace("\n ", '', $parts[$i]);
+                    $html = str_replace("\n\n", "\r", $parts[$i]);
+                    $html = str_replace("\n", " ", $parts[$i]);
+                } else {
+                    $html = preg_replace("/\n/", "\r", $parts[$i], 2);  
+                    $html = str_replace("\n ", '', $parts[$i]);
+                    $html = str_replace("\n\n", "\r", $parts[$i]);
+                    $html = str_replace("\n", " ", $parts[$i]);
+                }
+                $array[$i] = $html;
+            }
         ?>
         <div class="t m0 x5 hf yd7 ff2 fs9 fc2 sc0 ls0 ws0"
             style="white-space: normal; width: 2000px; bottom: 620px">
-            {!! nl2br(e($attitudeIndicator[1])) !!}
+            {!! nl2br(e($array[0])) !!}
         </div>
 
         <div class="t m2 xa h6 y5f ff3 fs2 fc0 sc0 ls0 ws0">Numerology Report</div>
@@ -337,14 +372,14 @@
     </div>
 </div>
 
-@if(count($attitudeIndicator) >= 2)
-@for ($i = 2; $i <= count($attitudeIndicator); $i++)
+@if(count($array) >= 2)
+@for ($i = 1; $i < count($array); $i++)
 <div id="pfc" class="pf w0 h0" data-page-no="9">
 <div class="pc pce w0 h0 opened">
     <img class="bi x0 y0 w1 h1" alt=""
         src="{{ asset('/' . $path . '/page-trang-trai.png') }}">
     <div class="t m0 x5 h12 yf3 ff3 fs4 fc2 sc0 ls0 ws0" style="width: 2000px; white-space: normal">
-        {!! nl2br(e($attitudeIndicator[$i])) !!}
+        {!! nl2br(e($array[$i])) !!}
     </div>
     <div class="t m2 xe h6 y5f ff3 fs2 fc0 sc0 ls0 ws0">Numerology Report</div>
     @include('footer', ['name' => $data['fullName'], 'date' => $data['dateOfBirth']])

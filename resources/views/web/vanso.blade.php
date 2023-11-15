55c9ac19a887436084a30adf786863b269f81920
ngan_ct <caothuongngan@gmail.com>
2023-11-15 22:23:23 +0700

    commit

--- resources/views/web/vanso.blade.php
+++ resources/views/web/vanso.blade.php
@@ -140,7 +140,7 @@
             }
         ?>
         <div class="t m0 x5 h9 yc7 ff4 fs4 fc2 sc0 ls0 ws0" style="width: 2000px; white-space: normal; text-align: justify;">
-            {!! nl2br(e($array[0])) !!}
+            {!! nl2br(e($first)) !!}
         </div>
 
         <div class="t m2 xa h6 y5f ff3 fs2 fc0 sc0 ls0 ws0">Numerology Report</div>

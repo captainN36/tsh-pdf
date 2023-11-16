2c845ca3532bb239dd8bb2d367baa63e6e74d7e9
mor-nganct <caothuongngan@gmail.com>
2023-11-16 10:44:45 +0700

    commit

--- resources/views/files/vanso.blade.php
+++ resources/views/files/vanso.blade.php
@@ -22,7 +22,7 @@
         <div class="t m0 x30 hf yab ff2 fs9 fc2 sc0 ls0 ws0" style="width: 1650px !important; white-space: normal;text-align: justify;">
             {!! substr($data['data']['cycleFortune']['description'], 0) !!}
         </div>
-        <div class="t m0 hf yaf ff2 fs9 fc2 sc0 ls0 ws0" style="display: flex; flex-wrap: wrap; left: 70px; top: 200px; width: 1950px">
+        <div class="t m0 hf yaf ff2 fs9 fc2 sc0 ls0 ws0" style="display: flex; flex-wrap: wrap; left: 70px; top: 300px; width: 1950px">
             {!! $data['data']['cycleFortune']['content'] !!}
         </div>
         @php
@@ -32,7 +32,7 @@
             });
             $number = array_values($filter)[0]['indicator'];
         @endphp
-        <div class="t m0 hf yab ff2 fs9 fc2 sc0 ls0 ws0" style="bottom: 70%; left: -60px">
+        <div class="t m0 hf yab ff2 fs9 fc2 sc0 ls0 ws0" style="bottom: 65%; left: -60px">
             @php
                 $fileName = $number - 1;
             @endphp

fa0c8d0b6d6ac32b7de9f8553ff3f4cec23739cc
mor-nganct <caothuongngan@gmail.com>
2023-11-16 10:38:02 +0700

    commit

--- resources/views/files/vanso.blade.php
+++ resources/views/files/vanso.blade.php
@@ -22,7 +22,7 @@
         <div class="t m0 x30 hf yab ff2 fs9 fc2 sc0 ls0 ws0" style="width: 1650px !important; white-space: normal;text-align: justify;">
             {!! substr($data['data']['cycleFortune']['description'], 0) !!}
         </div>
-        <div class="t m0 hf yaf ff2 fs9 fc2 sc0 ls0 ws0" style="display: flex; flex-wrap: wrap; left: 70px; bottom: 400px; width: 1950px">
+        <div class="t m0 hf yaf ff2 fs9 fc2 sc0 ls0 ws0" style="display: flex; flex-wrap: wrap; left: 70px; top: 0; width: 1950px">
             {!! $data['data']['cycleFortune']['content'] !!}
         </div>
         @php

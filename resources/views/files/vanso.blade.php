0f416bf5cd65320ac4a369f6d666910e62d19bb9
mor-nganct <caothuongngan@gmail.com>
2023-11-16 11:39:22 +0700

    commit

--- resources/views/files/vanso.blade.php
+++ resources/views/files/vanso.blade.php
@@ -32,7 +32,7 @@
             });
             $number = array_values($filter)[0]['indicator'];
         @endphp
-        <div class="t m0 hf yab ff2 fs9 fc2 sc0 ls0 ws0" style="top: 370px !important; left: -60px">
+        <div class="t m0 hf yab ff2 fs9 fc2 sc0 ls0 ws0" style="bottom: 800px !important; left: -60px">
             @php
                 $fileName = $number - 1;
             @endphp

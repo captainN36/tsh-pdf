77c1f7afdef167cbff7fff43bb6c21bf156ab55d
mor-nganct <caothuongngan@gmail.com>
2023-11-16 14:54:59 +0700

    commit

--- resources/views/files/duongdoi.blade.php
+++ resources/views/files/duongdoi.blade.php
@@ -108,7 +108,7 @@
             $lines = explode("\n", $inputString);
 
             $linesPerPartFirst = 25;
-            $linesPerPartRest = 80;
+            $linesPerPartRest = 75;
 
             $parts = [];
 

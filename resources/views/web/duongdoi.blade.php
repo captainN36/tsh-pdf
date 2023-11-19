86a444494db132d7d401b9b5849a9030afa379d2
ngan_ct <caothuongngan@gmail.com>
2023-11-19 12:02:50 +0700

    commit

--- resources/views/web/duongdoi.blade.php
+++ resources/views/web/duongdoi.blade.php
@@ -952,6 +952,9 @@
                         width: 96vw !important;
                     }
                 }
+                .bieudo {
+                    width: 96vw !important;
+                }
             </style>
             <div class="t m0 x3 hc y97 ff1 fs6 fc0 sc0 ls0 ws0 bieudo" style="display: flex;justify-content: space-between;align-items: center;left: 104px;bottom: 969px;gap: 110px;height: 700px;">
                 <div class="test" style="display: flex; flex: 1; text-align: center; margin: 0; padding: 0;">

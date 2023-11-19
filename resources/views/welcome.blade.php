e65edc20c69960e0083c633a510c247583c0d09b
ngan_ct <caothuongngan@gmail.com>
2023-11-19 19:03:19 +0700

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -58,7 +58,7 @@
                 {{ $name->{1} ?? null }}<span class="_ _21"></span>{{ $name->{4} ?? null }}<span class="_ _24" style="width: 201px"> </span>{{ $name->{7} ?? null }}<span class="_ _27" style="width: 1200px;"> </span><span style="top: -120px">{{ $data['data']['attitudeIndicator']['attitudeIndicator'] }}</span>
             </div>
             <div class="t m0 x51 h14 y1c8 ff1 fsc fc7 sc0 ls0 ws0" style="left: 40px;bottom: 820px">
-                {{ $summary->{3} ?? null }}<span class="_ _21"></span>{{ $summary->{6} ?? null }}<span class="_ _24"> </span>{{ $summary->{9} ?? null }}<span class="_ _27" style="width: 1250px;"> </span><span style="top: -145px">{{ $data['data']['soulIndicator']['soulIndicator'] }}</span>
+                {{ $summary->{3} ?? null }}<span class="_ _21"></span>{{ $summary->{6} ?? null }}<span class="_ _24"> </span>{{ $summary->{9} ?? null }}<span class="_ _27" style="width: 1225px;"> </span><span style="top: -145px">{{ $data['data']['soulIndicator']['soulIndicator'] }}</span>
             </div>
             <div class="t m0 x51 h14 y1c8 ff1 fse fc7 sc0 ls0 ws0" style="left: 45px;bottom: 750px">
                 {{ $summary->{2} ?? null }}<span class="_ _21"></span>{{ $summary->{5} ?? null }}<span class="_ _24"> </span>{{ $summary->{8} ?? null }}<span class="_ _27" style="width: 1200px;"> </span><span style="top: -190px">{{ implode(', ', $data['data']['passionIndicator']['passionIndicator']) }}</span>

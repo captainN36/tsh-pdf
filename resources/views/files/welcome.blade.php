f960796ba9498a81fe8abfce6d918efe9dd439bf
ngan_ct <caothuongngan@gmail.com>
2023-11-21 01:39:53 +0700

    commit

--- resources/views/files/welcome.blade.php
+++ resources/views/files/welcome.blade.php
@@ -1423,6 +1423,25 @@
         <div class="pc pcb w0 h0 opened">
             <img class="bi x0 y0 w1 h1" alt=""
                 src="{{ asset('/' . $path . '/page-trang-trai.png') }}">
+            <div class="t m0 x5 hf yd7 ff2 fs9 fc2 sc0 ls0 ws0" style="white-space: normal; width: 2360px; top: 0px">
+                @foreach ($strengthTWO as $item)
+                    {!! $item !!}
+                @endforeach
+            </div>
+
+            <div class="t m2 xe h6 y5f ff3 fs2 fc0 sc0 ls0 ws0">Numerology Report</div>
+            @include('footer', ['name' => $data['fullName'], 'date' => $data['dateOfBirth']])
+            <?php $page = $page + 1; ?>
+            <div class="t m0 x3b h5 y61 ff2 fs2 fc0 sc0 ls0 ws0">{{$page}}</div>
+        </div>
+        <div class="pi" data-data="{&quot;ctm&quot;:[1.500000,0.000000,0.000000,1.500000,0.000000,0.000000]}">
+        </div>
+    </div>
+
+    <div id="pfd" class="pf w0 h0" data-page-no="52">
+        <div class="pc pcb w0 h0 opened">
+            <img class="bi x0 y0 w1 h1" alt=""
+                src="{{ asset('/' . $path . '/page-trang-trai.png') }}">
             @php
                 $strengthTWO = [];
                 $jsonStrength =
@@ -1447,10 +1466,12 @@
                 }
             @endphp
             <div class="t m0 x5 hf yd7 ff2 fs9 fc2 sc0 ls0 ws0" style="white-space: normal; width: 2360px; top: 0px">
-                @foreach ($strengthTWO as $item)
-                    {!! $item !!}
+                @foreach ($data1 as $item)
+                    {!! $item['title'] !!}
+                    <p>{!! $item['content'] !!}</p>
                 @endforeach
             </div>
+            
 
             <div class="t m2 xe h6 y5f ff3 fs2 fc0 sc0 ls0 ws0">Numerology Report</div>
             @include('footer', ['name' => $data['fullName'], 'date' => $data['dateOfBirth']])

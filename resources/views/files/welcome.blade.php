0a6446d6b534f3ceb967495f59c01e73d8f7bfed
ngan_ct <caothuongngan@gmail.com>
2023-11-21 01:04:39 +0700

    commit

--- resources/views/files/welcome.blade.php
+++ resources/views/files/welcome.blade.php
@@ -1733,10 +1733,10 @@
                 {{ $name->{1} ?? null }}<span class="_ _21"></span>{{ $name->{4} ?? null }}<span class="_ _24" style="width: 160px"> </span>{{ $name->{7} ?? null }}<span class="_ _27" style="width: 1240px;"> </span><span style="top: -135px">{{ $data['data']['attitudeIndicator']['attitudeIndicator'] }}</span>
             </div>
             <div class="t m0 x51 h14 y1c8 ff1 fsc fc7 sc0 ls0 ws0" style="left: 35px;bottom: 1050px">
-                {{ $summary->{3} ?? null }}<span class="_ _21"></span>{{ $summary->{6} ?? null }}<span class="_ _24" style="width: 150px"> </span>{{ $summary->{9} ?? null }}<span class="_ _27" style="width: 1250px;"> </span><span style="top: -135px">{{ $data['data']['soulIndicator']['soulIndicator'] }}</span>
+                {{ $summary->{3} ?? null }}<span class="_ _21"></span>{{ $summary->{6} ?? null }}<span class="_ _24" style="width: 150px"> </span>{{ $summary->{9} ?? null }}<span class="_ _27" style="width: 1220px;"> </span><span style="top: -135px">{{ $data['data']['soulIndicator']['soulIndicator'] }}</span>
             </div>
             <div class="t m0 x51 h14 y1c8 ff1 fsc fc7 sc0 ls0 ws0" style="left: 35px;bottom: 980px">
-                {{ $summary->{2} ?? null }}<span class="_ _21" style="width: 110px"></span>{{ $summary->{5} ?? null }}<span class="_ _24" style="width: 130px"> </span>{{ $summary->{8} ?? null }}<span class="_ _27" style="width: 1270px;"> </span><span style="top: -200px">{{ implode(', ', $data['data']['passionIndicator']['passionIndicator']) }}</span>
+                {{ $summary->{2} ?? null }}<span class="_ _21" style="width: 110px"></span>{{ $summary->{5} ?? null }}<span class="_ _24" style="width: 130px"> </span>{{ $summary->{8} ?? null }}<span class="_ _27" style="width: 1200px;"> </span><span style="top: -200px">{{ implode(', ', $data['data']['passionIndicator']['passionIndicator']) }}</span>
             </div>
             <div class="t m0 x51 h14 y1c8 ff1 fsc fc7 sc0 ls0 ws0" style="left: 35px;bottom: 910px; width: 150px">
                 {{ $summary->{1} ?? null }}<span class="_ _21" style="width: 110px"></span>{{ $summary->{4} ?? null }}<span class="_ _24" style="width: 110px"> </span>{{ $summary->{7} ?? null }}<span class="_ _27" style="width: 1335px;"> </span><span style="top: -275px">{{ $data['data']['thinkingIndicator']['thinkingIndicator'] }}</span>

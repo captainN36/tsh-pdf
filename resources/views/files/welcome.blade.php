ba9195d233cb3da1f53ba6d723602d977c8f16e9
mor-nganct <caothuongngan@gmail.com>
2023-11-21 13:11:21 +0700

    commit

--- resources/views/files/welcome.blade.php
+++ resources/views/files/welcome.blade.php
@@ -1535,10 +1535,10 @@
                 src="{{ asset('/' . $path . '/ngay-sinh.png') }}">
             <div class="t m0 x13 hd y93 ff1 fs7 fc0 sc0 ls0 ws0"></div>
             <div class="t m0 x14 he y94 ff1 fs8 fc0 sc0 ls0 ws0">BIỂU ĐỒ TỔNG HỢP NGÀY SINH, HỌ TÊN VÀ NGHỆ DANH</div>
-            <div class="t m0 x51 h14 y1c7 ff1 fsc fc7 sc0 ls0 ws0" style="height: 60.5px; display: flex; left: 65px; width: 2360px; top: 50px">
+            <div class="t m0 x51 h14 y1c7 ff1 fsc fc7 sc0 ls0 ws0" style="height: 60.5px; left: 65px; width: 2360px; top: 50px">
                 <span style="color: unset"></span>{{ $name->{3} ?? "null" }}
-                <span style="color: unset; display: flex; left: -5px"></span> {{ $name->{6} ?? "null" }}
-                <span style="color: unset; display: flex; left: -5px"></span>{{ $name->{9} ?? "null" }}
+                <span style="color: unset"></span> {{ $name->{6} ?? "null" }}
+                <span style="color: unset"></span>{{ $name->{9} ?? "null" }}
                 <span style="color: unset; width: 40px" class="_ _24"></span>{{ $summary->{3} ?? "null" }}
                 <span style="color: unset; width: 10px" class="_ _24"> </span>{{ $summary->{6} ?? "null" }}
                 <span style="color: unset; width: 10px" class="_ _24"></span>{{ $summary->{9} ?? "null" }} 

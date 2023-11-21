0114f5b35cde95e003adfac0d4ddfee4a2939eec
ngan_ct <caothuongngan@gmail.com>
2023-11-21 22:45:47 +0700

    commit

--- resources/views/files/welcome.blade.php
+++ resources/views/files/welcome.blade.php
@@ -1536,12 +1536,12 @@
             <div class="t m0 x13 hd y93 ff1 fs7 fc0 sc0 ls0 ws0"></div>
             <div class="t m0 x14 he y94 ff1 fs8 fc0 sc0 ls0 ws0">BIỂU ĐỒ TỔNG HỢP NGÀY SINH, HỌ TÊN VÀ NGHỆ DANH</div>
             <div class="t m0 x51 h14 y1c7 ff1 fsc fc7 sc0 ls0 ws0" style="height: 60.5px; display: flex; left: 70px; width: 2160px">
-                <span style="color: unset; width: 5px" class="_ _24">{{ $name->{3} ?? "nil" }}</span>
-                <span style="color: unset; width: 5px; margin-left: 70px;" class="_ _24">{{ $name->{6} ?? "nil" }}</span>
-                <span style="color: unset; width: 5px; margin-left: 70px;" class="_ _22">{{ $name->{9} ?? "nil" }} </span>
-                <span style="color: unset; width: 350px; margin-left: 350px;" class="_ _24">{{ $summary->{3} ?? "nil" }}</span>
-                <span style="color: unset; width: 5px; margin-left: 70px;" class="_ _24">{{ $summary->{6} ?? "nil" }} </span>
-                <span style="color: unset; width: 5px; margin-left: 70px" class="_ _24">{{ $summary->{9} ?? "nil" }} </span>
+                <div style="color: unset" class="_ _24">{{ $name->{3} ?? "nil" }}</div>
+                <div style="color: unset;" class="_ _24">{{ $name->{6} ?? "nil" }}</div>
+                <div style="color: unset;" class="_ _22">{{ $name->{9} ?? "nil" }} </div>
+                <div style="color: unset; width: 350px; margin-left: 350px;" class="_ _24">{{ $summary->{3} ?? "nil" }}</div>
+                <div style="color: unset;" class="_ _24">{{ $summary->{6} ?? "nil" }} </div>
+                <div style="color: unset" class="_ _24">{{ $summary->{9} ?? "nil" }} </div>
             </div>
 
             <div class="t m0 x51 h14 y1c7 ff1 fsc fc7 sc0 ls0 ws0" style="height: 60.5px;display: flex;top: 198px; left: 70px; width: 2360px">

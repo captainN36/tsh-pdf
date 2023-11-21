0fb16154544e90bca4859171f2339a8870003969
ngan_ct <caothuongngan@gmail.com>
2023-11-21 23:10:04 +0700

    commit

--- resources/views/files/welcome.blade.php
+++ resources/views/files/welcome.blade.php
@@ -1537,7 +1537,7 @@
             <div class="t m0 x14 he y94 ff1 fs8 fc0 sc0 ls0 ws0">BIỂU ĐỒ TỔNG HỢP NGÀY SINH, HỌ TÊN VÀ NGHỆ DANH</div>
             <div class="t m0 x51 h14 y1c7 ff1 fsc fc7 sc0 ls0 ws0" style="height: 60.5px; display: flex; left: 70px; width: 2160px">
                 <div style="color: unset" class="_ _24">{{ $name->{3} ?? "nil" }}</div>
-                <div style="color: unset; margin-left: -20px" class="_ _24">{{ $name->{6} ?? "nil" }}</div>
+                <div style="color: unset; left: -20px" class="_ _24">{{ $name->{6} ?? "nil" }}</div>
                 <div style="color: unset;" class="_ _22">{{ $name->{9} ?? "nil" }} </div>
                 <div style="color: unset; width: 350px; margin-left: 350px;" class="_ _24">{{ $summary->{3} ?? "nil" }}</div>
                 <div style="color: unset;" class="_ _24">{{ $summary->{6} ?? "nil" }} </div>

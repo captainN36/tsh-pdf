66939c775eef20212646c2bf74b4aa1f42d4edbf
ngan_ct <caothuongngan@gmail.com>
2023-11-22 01:06:11 +0700

    commit

--- resources/views/files/welcome.blade.php
+++ resources/views/files/welcome.blade.php
@@ -1536,29 +1536,29 @@
             <div class="t m0 x13 hd y93 ff1 fs7 fc0 sc0 ls0 ws0"></div>
             <div class="t m0 x14 he y94 ff1 fs8 fc0 sc0 ls0 ws0">BIỂU ĐỒ TỔNG HỢP NGÀY SINH, HỌ TÊN VÀ NGHỆ DANH</div>
             <div class="t m0 x51 h14 y1c7 ff1 fsc fc7 sc0 ls0 ws0" style="height: 60.5px; display: flex; left: 70px; width: 2360px">
-                <span style="color: unset; width: 5px" class="_ _24">{{ $name->{3} ?? "nil" }}</span>
-                <span style="color: unset; width: 5px" class="_ _24">{{ $name->{6} ?? "nil" }}</span>
+                <span style="color: unset; width: 5px" class="_ _24">{{ $name->{3} ?? null }}</span>
+                <span style="color: unset; width: 5px" class="_ _24">{{ $name->{6} ?? null }}</span>
                 <span style="color: unset; width: 5px" class="_ _22">{{ $name->{9} ?? null }} </span>
-                <span style="color: unset; width: 10px; margin-left: 350px;" class="_ _24">{{ $summary->{3} ?? "nil" }}</span>
-                <span style="color: unset; width: 5px" class="_ _24">{{ $summary->{6} ?? "nil" }} </span>
-                <span style="color: unset; width: 5px" class="_ _24">{{ $summary->{9} ?? "nil" }} </span>
+                <span style="color: unset; width: 10px; margin-left: 350px;" class="_ _24">{{ $summary->{3} ?? null }}</span>
+                <span style="color: unset; width: 5px" class="_ _24">{{ $summary->{6} ?? null }} </span>
+                <span style="color: unset; width: 5px" class="_ _24">{{ $summary->{9} ?? null }} </span>
             </div>
 
             <div class="t m0 x51 h14 y1c7 ff1 fsc fc7 sc0 ls0 ws0" style="height: 60.5px;display: flex;top: 198px; left: 70px; width: 2360px">
-                <span style="color: unset; width: 5px" class="_ _24">{{ $name->{2} ?? "nil" }}</span>
-                <span style="color: unset; width: 5px" class="_ _24">{{ $name->{5} ?? "nil" }}</span>
+                <span style="color: unset; width: 5px" class="_ _24">{{ $name->{2} ?? null }}</span>
+                <span style="color: unset; width: 5px" class="_ _24">{{ $name->{5} ?? null }}</span>
                 <span style="color: unset; width: 5px" class="_ _22">{{ $name->{8} ?? null }} </span>
-                <span style="color: unset; width: 10px; margin-left: 350px;" class="_ _24">{{ $summary->{2} ?? "nil" }}</span>
-                <span style="color: unset; width: 5px" class="_ _24">{{ $summary->{5} ?? "nil" }} </span>
-                <span style="color: unset; width: 5px" class="_ _24">{{ $summary->{8} ?? "nil" }} </span>
+                <span style="color: unset; width: 10px; margin-left: 350px;" class="_ _24">{{ $summary->{2} ?? null }}</span>
+                <span style="color: unset; width: 5px" class="_ _24">{{ $summary->{5} ?? null }} </span>
+                <span style="color: unset; width: 5px" class="_ _24">{{ $summary->{8} ?? null }} </span>
             </div>
             <div class="t m0 x51 h14 y1c7 ff1 fsc fc7 sc0 ls0 ws0" style="height: 60.5px;display: flex;top: 260px; left: 70px; width: 2360px">
-                <span style="color: unset; width: 5px" class="_ _24">{{ $name->{1} ?? "nil" }}</span>
-                <span style="color: unset; width: 5px" class="_ _24"> {{ $name->{4} ?? "nil" }}</span>
-                <span style="color: unset; width: 5px" class="_ _22">{{ $name->{7} ?? "nil" }} </span>
-                <span style="color: unset; width: 10px; margin-left: 350px;" class="_ _24">{{ $summary->{1} ?? "nil" }}</span>
-                <span style="color: unset; width: 5px" class="_ _24">{{ $summary->{4} ?? "nil" }} </span>
-                <span style="color: unset; width: 5px" class="_ _24">{{ $summary->{7} ?? "nil" }} </span>
+                <span style="color: unset; width: 5px" class="_ _24">{{ $name->{1} ?? null }}</span>
+                <span style="color: unset; width: 5px" class="_ _24"> {{ $name->{4} ?? null }}</span>
+                <span style="color: unset; width: 5px" class="_ _22">{{ $name->{7} ?? null }} </span>
+                <span style="color: unset; width: 10px; margin-left: 350px;" class="_ _24">{{ $summary->{1} ?? null }}</span>
+                <span style="color: unset; width: 5px" class="_ _24">{{ $summary->{4} ?? null }} </span>
+                <span style="color: unset; width: 5px" class="_ _24">{{ $summary->{7} ?? null }} </span>
             </div>
 
             @php

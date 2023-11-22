c71fefe0182c850d9fccb331dc97d3b722f7b081
ngan_ct <caothuongngan@gmail.com>
2023-11-23 02:04:22 +0700

    commit

--- resources/views/files/welcome.blade.php
+++ resources/views/files/welcome.blade.php
@@ -1859,7 +1859,7 @@
             <?php $array_2 = array(1, 2, 3, 4); ?>
 
             <div class="t m0 x51 h14 y1c8 ff1 fsc fc7 sc0 ls0 ws0" style="left: 45px; bottom: 375px">
-                <span class="_ _27" style="@if(count($array_1) > 4) width: 1860px @else width: 1920px; @endif"> </span><span style="bottom: 810px; font-size: 41px">{{ implode(', ', array_slice($array_2, 0, 5)) }}</span>@if(count($array_1) > 4)<span style="bottom: 820px; font-size: 45px; display: flex; left: 2160px">{{ implode(', ', array_slice($array_2, 5)) }}</span>@endif
+                <span class="_ _27" style="@if(count($array_1) > 4) width: 1860px @else width: 1900px; @endif"> </span><span style="bottom: 810px; font-size: 41px">{{ implode(', ', array_slice($array_2, 0, 5)) }}</span>@if(count($array_1) > 4)<span style="bottom: 820px; font-size: 45px; display: flex; left: 2160px">{{ implode(', ', array_slice($array_2, 5)) }}</span>@endif
                 
             </div>
             <div class="t m0 x51 h14 y1c8 ff1 fsc fc7 sc0 ls0 ws0" style="left: 45px; bottom: 335px">

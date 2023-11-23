2599d780b174b12d0b36d484a25232a74803af6a
mor-nganct <caothuongngan@gmail.com>
2023-11-23 17:00:45 +0700

    commit

--- resources/views/files/welcome.blade.php
+++ resources/views/files/welcome.blade.php
@@ -1840,7 +1840,7 @@
             </div>
             <?php
                 // $array_1 = $data['data']['passionIndicator']['passionIndicator'];
-                $array_1 = [1,2,3,4];
+                $array_1 = [1,2,3,4,6,7,8,9];
                 switch(count($array_1)) {
                     case 1:
                         $width = 1470;

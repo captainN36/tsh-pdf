a1417b8161a506ae4dedcefbacd8b2a373608905
ngan_ct <caothuongngan@gmail.com>
2023-11-25 00:53:11 +0700

    commit

--- resources/views/web/welcome.blade.php
+++ resources/views/web/welcome.blade.php
@@ -223,7 +223,7 @@
         <img class="bi x0 y0 w1 h1" alt=""
             src="{{ asset('/' . $path . '/page-trang-phai.png') }}">
         <div class="t m0 x5 hf yf3 ff4 fs9 fc2 sc0 ls0 ws0" style="width: 2000px; white-space: normal; text-align: justify;">
-                        {!! $nextYearIndicator[$i] !!}
+                        {!! nl2br(e($nextYearIndicator[$i])) !!}
         </div>
 
         <div class="t m2 xa h6 y5f ff3 fs2 fc0 sc0 ls0 ws0">Numerology Report</div>

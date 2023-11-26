1d1ae9ecbe77c0a252fab24239fcfc69566886f8
ngan_ct <caothuongngan@gmail.com>
2023-11-27 01:38:17 +0700

    commit

--- resources/views/files/welcome.blade.php
+++ resources/views/files/welcome.blade.php
@@ -245,8 +245,6 @@
     for ($i = 1; $i < count($parts); $i++) {
         $html = str_replace("\n\n", "\r", $parts[$i]);
         $html = str_replace("\n", " ", $parts[$i]);
-        $html = str_replace("<br>", " ", $parts[$i]);
-        $html = str_replace("\n", " ", $parts[$i]);
         $array[$i] = $html;
     }
 ?>

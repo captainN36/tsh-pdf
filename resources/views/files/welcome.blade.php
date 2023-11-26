7355dfae4e077d1fbe79d61e65683990a77e677f
ngan_ct <caothuongngan@gmail.com>
2023-11-27 01:28:45 +0700

    commit

--- resources/views/files/welcome.blade.php
+++ resources/views/files/welcome.blade.php
@@ -246,6 +246,7 @@
         $html = str_replace("\n\n", "\r", $parts[$i]);
         $html = str_replace("\n", "", $parts[$i]);
         $html = str_replace("<br>", "\r", $parts[$i]);
+        dd($html);
         $array[$i] = $html;
     }
     dd($array);

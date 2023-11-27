e0265f67757c1d8bab7f5c84fe9215426f85496c
mor-nganct <caothuongngan@gmail.com>
2023-11-27 14:24:55 +0700

    commit

--- resources/views/files/welcome.blade.php
+++ resources/views/files/welcome.blade.php
@@ -188,6 +188,26 @@
         <div class="pi" data-data="{&quot;ctm&quot;:[1.500000,0.000000,0.000000,1.500000,0.000000,0.000000]}"></div>
     </div>
 </div>
+
+@if(count($parts) >= 2)
+@for ($i = 1 ; $i < count($parts) ; $i++)
+<?php $page++; ?>
+    <div id="pfd" class="pf w0 h0" data-page-no="18">
+    <div class="pc pcb w0 h0 opened">
+        <img class="bi x0 y0 w1 h1" alt=""
+            src="{{ asset('/' . $path . '/page-trang-trai.png') }}">
+        <div class="t m0 x5 hf yf3 ff4 fs9 fc2 sc0 ls0 ws0" style="width: 2360px; white-space: normal; text-align: justify;">
+            {!! nl2br(e($parts[$i])) !!}
+        </div>
+        <div class="t m2 xe h6 y5f ff3 fs2 fc0 sc0 ls0 ws0">Numerology Report</div>
+        @include('footer', ['name' => $data['fullName'], 'date' => $data['dateOfBirth']])
+        <div class="t m0 x3b h5 y61 ff2 fs2 fc0 sc0 ls0 ws0">{{$page}}</div>
+    </div>
+    <div class="pi" data-data="{&quot;ctm&quot;:[1.500000,0.000000,0.000000,1.500000,0.000000,0.000000]}"></div>
+</div>
+@endfor
+@endif
+
 </div>
 </body>
 

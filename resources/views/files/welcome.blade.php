9a3ad3c2b6fbf1609d6c85054d3fb72ea7ffef45
mor-nganct <caothuongngan@gmail.com>
2023-11-27 16:38:49 +0700

    commit

--- resources/views/files/welcome.blade.php
+++ resources/views/files/welcome.blade.php
@@ -525,6 +525,7 @@
             justify-content: center;
             align-items: center;
             width: {{ $data['data']['percentIndicator'][8][1] == $max ? 2280 : ( 2280/100) * (($data['data']['percentIndicator'][8][1]/$max) * 100) }}px;
+            bottom: 180px;
             left: 110px;
         }
         @endisset

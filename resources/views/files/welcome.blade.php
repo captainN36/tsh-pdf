2841323bb127eacd6a543809787da499cbc49534
mor-nganct <caothuongngan@gmail.com>
2023-11-29 16:45:33 +0700

    commit

--- resources/views/files/welcome.blade.php
+++ resources/views/files/welcome.blade.php
@@ -67,7 +67,7 @@
                 });
                 $number = array_values($filter)[0]['indicator'];
             @endphp
-            <div class="t m0 hb y276 ff2 fs9 fc2 sc0 ls0 ws0 x16-minus" style="left: -75.253333px !important; bottom: 700px">
+            <div class="t m0 hb y276 ff2 fs9 fc2 sc0 ls0 ws0 x16-minus" style="left: -75.253333px !important; bottom: 800px">
                 @php
                     $fileName = $number - 1;
                 @endphp

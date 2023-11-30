4a09539198e1ea294ce26608e89fa92ca8a3ead9
mor-nganct <caothuongngan@gmail.com>
2023-11-30 14:31:49 +0700

    commit

--- resources/views/files/welcome.blade.php
+++ resources/views/files/welcome.blade.php
@@ -1288,8 +1288,6 @@
             <div class="t m0 x14 he y94 ff1 fs8 fc0 sc0 ls0 ws0">SỐ BẢN CHẤT CỦA BẠN</div>
             <div class="t m0 h7 yd5 ff1 fs3 fc7 sc0 ls0 ws0 index-center" style="width: 2800px !important">
                 {{ $data['data']['natureIndicator']['natureIndicator'] }}</div>
-        
-
             <div class="t m0 x5 hf yd7 ff4 fs9 fc2 sc0 ls0 ws0"
                  style="white-space: normal; width: 2360px; bottom: 1010px; text-align: justify;">
                  {!! $data['data']['natureIndicator']['description'] !!}

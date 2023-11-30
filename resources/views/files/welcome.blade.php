d604b7ff6d63313c8b36981043cdf83a80c82434
mor-nganct <caothuongngan@gmail.com>
2023-11-30 14:36:45 +0700

    commit

--- resources/views/files/welcome.blade.php
+++ resources/views/files/welcome.blade.php
@@ -1290,7 +1290,7 @@
                 {{ $data['data']['natureIndicator']['natureIndicator'] }}</div>
             <div class="t m0 x5 hf yd7 ff4 fs9 fc2 sc0 ls0 ws0"
                  style="white-space: normal; width: 2360px; bottom: 1010px; text-align: justify;">
-                
+                 {!! $data['data']['natureIndicator']['data'][0] !!}
             </div>
 
             <div class="t m2 xa h6 y5f ff3 fs2 fc0 sc0 ls0 ws0">Numerology Report</div>

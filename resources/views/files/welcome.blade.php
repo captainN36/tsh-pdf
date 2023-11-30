0f5fc947e50d036d97c3c51b59626f5dd8151d37
mor-nganct <caothuongngan@gmail.com>
2023-11-30 14:29:14 +0700

    commit

--- resources/views/files/welcome.blade.php
+++ resources/views/files/welcome.blade.php
@@ -1288,13 +1288,12 @@
             <div class="t m0 x14 he y94 ff1 fs8 fc0 sc0 ls0 ws0">SỐ BẢN CHẤT CỦA BẠN</div>
             <div class="t m0 h7 yd5 ff1 fs3 fc7 sc0 ls0 ws0 index-center" style="width: 2800px !important">
                 {{ $data['data']['natureIndicator']['natureIndicator'] }}</div>
-            <div class="t m0 x5 hf yd6 ff2 fs9 fc2 sc0 ls0 ws0" style="white-space: normal; width: 2360px; text-align: justify;">
-                {!! $data['data']['natureIndicator']['description'] !!}
-            </div>
+        
 
             <div class="t m0 x5 hf yd7 ff4 fs9 fc2 sc0 ls0 ws0"
-                 style="white-space: normal; width: 2360px; bottom: 800px; text-align: justify;">
-                {!! nl2br(e($array[0])) !!}
+                 style="white-space: normal; width: 2360px; bottom: 1010px; text-align: justify;">
+                 {!! $data['data']['natureIndicator']['description'] !!}
+                 {!! $data['data']['natureIndicator']['data'][0] !!}
             </div>
 
             <div class="t m2 xa h6 y5f ff3 fs2 fc0 sc0 ls0 ws0">Numerology Report</div>

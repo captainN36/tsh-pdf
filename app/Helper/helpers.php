c8ea4b88bcf9502afcb76b7c587f8d69c28c400c
nganct <nganct@mor.com.vn>
2024-03-21 17:08:25 +0700

    commit

--- app/Helper/helpers.php
+++ app/Helper/helpers.php
@@ -12,12 +12,12 @@ if (!function_exists('contentText')) {
         }
 
         $pages =  explode('<></>', $content);
-        for ($i = 0; $i < count($pages); $i++) {
-            if ($i == 0) {
-                $string = preg_replace('/<p><br><\/p>/', '', $pages[0], 1);
-                $pages[0] = $string;
-            }
-        }
+        // for ($i = 0; $i < count($pages); $i++) {
+        //     if ($i == 0) {
+        //         $string = preg_replace('/<p><br><\/p>/', '', $pages[0], 1);
+        //         $pages[0] = $string;
+        //     }
+        // }
 
         return $pages;
     }

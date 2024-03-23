8f0d4a498f3e9a62683d544133e694c044e1e6ff
ngan_ct <caothuongngan@gmail.com>
2024-03-24 02:27:42 +0700

    commit

--- app/Helper/helpers.php
+++ app/Helper/helpers.php
@@ -21,13 +21,14 @@ if (!function_exists('textFromArray')) {
     function textFromArray($array)
     {
         $concatenated_string = implode(" <br>", $array);
-
+        $data = [];
         while (strlen($concatenated_string) > 3144) {
+            $data[] = $array[count($array) - 1];
             array_pop($array);
             $concatenated_string = implode(" <br>", $array);
         }
 
-        return $concatenated_string;
+        return [$concatenated_string, ];
     }
 }
 

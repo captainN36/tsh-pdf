24e7416a76d95a8eff76d236b7d84b183a605ebb
ngan_ct <caothuongngan@gmail.com>
2024-03-25 00:30:31 +0700

    commit

--- resources/views/files/welcome.blade.php
+++ resources/views/files/welcome.blade.php
@@ -1489,7 +1489,8 @@
                     {{ implode(', ', $data['data']['repeatIndicator']['repeatIndicator']) }}
                 </div>
                 <?php
-                    $repeatIndicator_first = contentText($data['data']['repeatIndicator']['firstContent']);
+                    $repeatIndicator = contentText($data['data']['repeatIndicator']['firstContent']);
+                    [$textFromArray, $nextPageContent] = getTextData($repeatIndicator, $data['data']['repeatIndicator']);
                 ?>
                 <div class="t m0 x5 hf yd7 ff2 fs4 fc2 sc0 ls0 ws0"
                     style="white-space: normal; width: 2360px; bottom: 1050px; text-align: justify;">
@@ -1497,12 +1498,7 @@
                     <br>
                     <div id="lpc" class="ff4 fs4 fc2 sc0 ls0 ws0"
                         style="white-space: normal; width: 2360px; text-align: justify;">
-                        {!! $repeatIndicator_first[0] !!} <br>
-                        @for ($i = 1; $i < count($data['data']['repeatIndicator']['data']); $i++)
-                            @if (isset($data['data']['repeatIndicator']['data'][$i]))
-                                {!! $data['data']['repeatIndicator']['data'][$i] !!} <br>
-                            @endif
-                        @endfor
+                        {!! $textFromArray !!}
                     </div>
                 </div>
 
@@ -1515,6 +1511,27 @@
                 </div>
             </div>
         </div>
+
+        @while (!empty($nextPageContent))
+            <?php
+                $page++; 
+                [$nextText, $nextPageContent] = textFromArray($nextPageContent, true);
+            ?>
+            <div id="pfc" class="pf w0 h0" data-page-no="9">
+                <div class="pc pce w0 h0 opened">
+                    <img class="bi x0 y0 w1 h1" alt=""
+                        src="{{ asset('/' . $path . '/page-trang-trai.png') }}">
+                    <div class="t m0 x5 h12 yf3 ff4 fs4 fc2 sc0 ls0 ws0"
+                        style="width: 2360px; white-space: normal; text-align: justify;">
+                        {!! $nextText !!}
+                    </div>
+                    <div class="t m2 xe h6 y5f ff3 fs2 fc0 sc0 ls0 ws0">Numerology Report</div>
+                    @include('footer', ['name' => $data['fullName'], 'date' => $data['dateOfBirth']])
+                    <div class="t m0 x3b h5 y61 ff2 fs2 fc0 sc0 ls0 ws0">{{ $page }}</div>
+                </div>
+                <div class="pi" data-data="{&quot;ctm&quot;:[1.500000,0.000000,0.000000,1.500000,0.000000,0.000000]}"></div>
+            </div>
+        @endwhile
 
         @if (isset($data['data']['missIndicator']))
             <div id="pf7-8" class="pf w0 h0" data-page-no="22">

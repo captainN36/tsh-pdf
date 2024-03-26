917780076db7c3e9cdc21ded08de16ffd5c875cc
ngan_ct <caothuongngan@gmail.com>
2024-03-27 01:26:55 +0700

    commit

--- resources/views/files/welcome.blade.php
+++ resources/views/files/welcome.blade.php
@@ -1519,7 +1519,7 @@
                     <div class="pc pce w0 h0 opened">
                         <img class="bi x0 y0 w1 h1" alt=""
                             src="{{ asset('/' . $path . '/page-trang-trai.png') }}">
-                        <div class="t m0 x5 h12 yf3 ff4 fs4 fc2 sc0 ls0 ws0"
+                        <div class="t m0 x5 h12 yf3 ff3 fs4 fc2 sc0 ls0 ws0"
                             style="width: 2360px; white-space: normal; text-align: justify;">
                             {!! $nextText !!}
                         </div>
@@ -1596,8 +1596,6 @@
                     {{ $data['data']['linkLifePathAndMission']['linkLifePathAndMission'] }}
                 </div>
                 <?php
-                // $linkLifePathAndMission_description = \App\Http\Controllers\PDFController::renderText($data['id'] . '-' . $data['dateSearch'] . '-' . 'linkLifePathAndMission_description', $data['data']['linkLifePathAndMission']['description'], false);
-                // $linkLifePathAndMission = \App\Http\Controllers\PDFController::renderText($data['id'] . '-' . $data['dateSearch'] . '-' . 'linkLifePathAndMission', $data['data']['linkLifePathAndMission']['content'], false);
                 $linkLifePathAndMission = contentText($data['data']['linkLifePathAndMission']['content']);
                 ?>
                 <div class="t m0 x5 hf yd7 ff2 fs4 fc2 sc0 ls0 ws0"
@@ -1987,8 +1985,10 @@
                 @endphp
                 <div class="t m0 x5 hf yd7 ff2 fs9 fc2 sc0 ls0 ws0"
                     style="white-space: normal; width: 2360px; top: 0px; text-align: justify">
-                    @foreach ($strengthTWO as $item)
-                        {!! $item !!}
+                    @foreach ($strengthTWO as $key => $item)
+                        @if ($key != 4)
+                            {!! $item !!}
+                        @endif
                     @endforeach
                 </div>
 
@@ -2000,6 +2000,27 @@
             <div class="pi" data-data="{&quot;ctm&quot;:[1.500000,0.000000,0.000000,1.500000,0.000000,0.000000]}">
             </div>
         </div>
+        <div id="pfd" class="pf w0 h0" data-page-no="52">
+            <div class="pc pcb w0 h0 opened">
+                <img class="bi x0 y0 w1 h1" alt="" src="{{ asset('/' . $path . '/page-trang-trai.png') }}">
+                <div class="t m0 x5 hf yd7 ff2 fs9 fc2 sc0 ls0 ws0"
+                    style="white-space: normal; width: 2360px; top: 0px; text-align: justify">
+                    @foreach ($strengthTWO as $key => $item)
+                        @if ($key == 4)
+                        {!! $item !!}
+                        @endif
+                    @endforeach
+                </div>
+
+                <div class="t m2 xe h6 y5f ff3 fs2 fc0 sc0 ls0 ws0">Numerology Report</div>
+                @include('footer', ['name' => $data['fullName'], 'date' => $data['dateOfBirth']])
+                <?php $page = $page + 1; ?>
+                <div class="t m0 x3b h5 y61 ff2 fs2 fc0 sc0 ls0 ws0">{{ $page }}</div>
+            </div>
+            <div class="pi" data-data="{&quot;ctm&quot;:[1.500000,0.000000,0.000000,1.500000,0.000000,0.000000]}">
+            </div>
+        </div>
+
 
         <div id="pf7" class="pf w0 h0" data-page-no="53">
             <div class="pc pc6 w0 h0 opened">

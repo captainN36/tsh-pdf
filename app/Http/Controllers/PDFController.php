f25890ab42e842a55e72913f4118a8b9982dbf34
ngan_ct <caothuongngan@gmail.com>
2024-04-11 22:06:16 +0700

    replace url

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -17,17 +17,13 @@ use \stdClass;
 class PDFController extends Controller
 {
 
-    public function index(Request $request)
+    public function index()
     {
         $params = [
             'url' => 'https://tsh.gemduck.tech/api/user/look-up-pdf/36013745-fc87-46d7-9194-6cb4d8c014d6',
             'token' => 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEwNywicm9sZSI6IkFETUlOIiwiaWF0IjoxNzExNjA2MjMxLCJleHAiOjE3MTQxOTgyMzF9.uFWy62vqOSrOePj4U3NZyMO8TL6xN3U7y-jusa2DW9A'
         ];
-        $params = $request->all();
-        $params['url'] = str_replace('look-up', 'look-up-pdf-test', $params['url']);
-
         $data = $this->getData($params);
-        dd($params, $data);
         return view('web.welcome', ['data' => $data]);
     }
     public function view(Request $request)

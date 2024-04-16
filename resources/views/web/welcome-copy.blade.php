cb921a416c04d358beb12c1e2efa2a85a04a942a
Your Name <you@example.com>
2024-04-16 10:33:49 +0700

    commit

--- resources/views/web/welcome-copy.blade.php
+++ resources/views/web/welcome-copy.blade.php
@@ -86,7 +86,7 @@
                     });
                     $number = array_values($filter)[0]['indicator'];
                 @endphp
-                <div class="t m0 hb y276 ff2 fs9 fc2 sc0 ls0 ws0 x16-minus" style="left: -75.253333px !important;">
+                <div class="t m0 hb y276 ff2 fs9 fc2 sc0 ls0 ws0 x16-minus" style="left: -135.253333px !important;">
                     @php
                         $fileName = $number - 1;
                     @endphp
@@ -118,22 +118,20 @@
                 </div>
                 <div class="t m0 x1a h9 yb9 ff4 fs4 fc2 sc0 ls0 ws0">Chu kỳ vận số của bạn</div>
                 <div class="t m0 x5 h12 yba ff3 fs4 fc2 sc0 ls0 ws0">Năm<span class="_ _a"></span> nay<span
-                        class="_ _a"></span> 2023,<span class="_ _a"></span> bạn<span class="_ _9"></span> có<span
-                        class="_ _a"></span> chỉ<span class="_ _a"></span> số<span class="_ _a"></span> năm<span
-                        class="_ _a"></span> (vận<span class="_ _a"></span> niên<span class="_ _a"></span> cá<span
-                        class="_ _9"></span> nhân)<span class="_ _a"></span> là<span class="_ _a"></span> 2<span
-                        class="_ _a"></span> và<span class="_ _a"></span> đang<span class="_ _a"></span> ở<span
-                        class="_ _a"></span> giai<span class="_ _a"></span> đoạn<span class="_ _9"></span> đầu
-                </div>
-                <div class="t m0 x5 h12 ybb ff3 fs4 fc2 sc0 ls0 ws0">của<span class="_ _d"></span> chu<span
-                        class="_ _b"></span> kỳ<span class="_ _b"></span> vận<span class="_ _b"></span> số.<span
-                        class="_ _d"></span> Xem<span class="_ _b"></span> thêm<span class="_ _b"></span>
-                    Phần<span class="_ _b"></span> B<span class="_ _d"></span> Mục<span class="_ _b"></span>
-                    7<span class="_ _b"></span> "CHỈ<span class="_ _b"></span> SỐ<span class="_ _d"></span>
-                    CÁC<span class="_ _b"></span> NĂM"<span class="_ _b"></span> để<span class="_ _d"></span>
-                    biết<span class="_ _b"></span> hướng<span class="_ _b"></span> phát</div>
-                <div class="t m0 x5 h12 ybb ff3 fs4 fc2 sc0 ls0 ws0" style="bottom: 230px">triển trong năm nay và 2 năm
-                    tới!
+                    class="_ _a"></span> {{date("Y")}},<span class="_ _a"></span> bạn<span class="_ _9"></span> có<span
+                    class="_ _a"></span> chỉ<span class="_ _a"></span> số<span class="_ _a"></span> năm<span
+                    class="_ _a"></span> (vận<span class="_ _a"></span> niên<span class="_ _a"></span> cá<span
+                    class="_ _9"></span> nhân)<span class="_ _a"></span> là<span class="_ _a"></span> 2<span
+                    class="_ _a"></span> và<span class="_ _a"></span> đang<span class="_ _a"></span> ở<span
+                    class="_ _a"></span> giai<span class="_ _a"></span> đoạn<span class="_ _9"></span> đầu</div>
+                    <div class="t m0 x5 h12 ybb ff3 fs4 fc2 sc0 ls0 ws0">của<span class="_ _d"></span> chu<span
+                    class="_ _b"></span> kỳ<span class="_ _b"></span> vận<span class="_ _b"></span> số.<span
+                    class="_ _d"></span> Xem<span class="_ _b"></span> thêm<span class="_ _b"></span> Phần<span
+                    class="_ _b"></span> B<span class="_ _d"></span> Mục<span class="_ _b"></span> 7<span
+                    class="_ _b"></span> "CHỈ<span class="_ _b"></span> SỐ<span class="_ _d"></span> CÁC<span
+                    class="_ _b"></span> NĂM"<span class="_ _b"></span> để<span class="_ _d"></span> biết<span
+                    class="_ _b"></span> hướng<span class="_ _b"></span> phát</div>
+                    <div class="t m0 x5 h12 ybb ff3 fs4 fc2 sc0 ls0 ws0" style="bottom: 230px">triển trong năm nay và 2 năm tới!</div>
                 </div>
                 <div class="t m2 xe h6 y5f ff3 fs2 fc0 sc0 ls0 ws0">Numerology Report</div>
                 @include('footer', ['name' => $data['fullName'], 'date' => $data['dateOfBirth']])

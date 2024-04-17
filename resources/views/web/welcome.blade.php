bee6340fd4e70d236f36151edf3714b1685f9590
ngan_ct <caothuongngan@gmail.com>
2024-04-18 00:05:05 +0700

    commit

--- resources/views/web/welcome.blade.php
+++ resources/views/web/welcome.blade.php
@@ -1219,7 +1219,7 @@
                 <div class="t m0 x13 hd y93 ff1 fs7 fc0 sc0 ls0 ws0 c-header-index">15</div>
                 <div class="t m0 x14 he y94 ff1 fs8 fc0 sc0 ls0 ws0">CHU KỲ ĐƯỜNG ĐỜI</div>
                 <?php
-                function lifeCircleIndicator()
+                function lifeCircleIndicator($data)
                 {
                     $data['data']['lifeCircleIndicator']['lifeCircleIndicator']['firstLifeCircle']['indicator'];
                     $data['data']['lifeCircleIndicator']['lifeCircleIndicator']['secondLifeCircle']['indicator'];
@@ -1244,8 +1244,8 @@
                 <div class="t m0 x35 h14 y126 ff1 fsc fc8 sc0 ls0 ws0">GIEO HẠT<span class="_ _1e"> </span><span
                         class="fc9">CHÍN<span class="_ _1f"> </span><span class="fca">THU HOẠCH</span></span>
                 </div>
-                <div class="t m0 x3e h6 y127 ff3 fs2 fc2 sc0 ls0 ws0">Đầu đời - {{$data['data']['lifeCircleIndicator']['lifeCircleIndicator']['firstLifeCircle']['age'][1]}} tuổi ({{lifeCircleIndicator()['first']}})<span class="_ _20">
-                    </span>{{$data['data']['lifeCircleIndicator']['lifeCircleIndicator']['secondLifeCircle']['age'][0]}} - {{$data['data']['lifeCircleIndicator']['lifeCircleIndicator']['secondLifeCircle']['age'][1]}} tuổi ({{lifeCircleIndicator()['second'][0]}} - {{lifeCircleIndicator()['second'][1]}})<span class="_ _21"> </span>{{$data['data']['lifeCircleIndicator']['lifeCircleIndicator']['thirdLifeCircle']['age'][0]}} tuổi về sau ({{lifeCircleIndicator()['third']}} trở đi)</div>
+                <div class="t m0 x3e h6 y127 ff3 fs2 fc2 sc0 ls0 ws0">Đầu đời - {{$data['data']['lifeCircleIndicator']['lifeCircleIndicator']['firstLifeCircle']['age'][1]}} tuổi ({{lifeCircleIndicator($data)['first']}})<span class="_ _20">
+                    </span>{{$data['data']['lifeCircleIndicator']['lifeCircleIndicator']['secondLifeCircle']['age'][0]}} - {{$data['data']['lifeCircleIndicator']['lifeCircleIndicator']['secondLifeCircle']['age'][1]}} tuổi ({{lifeCircleIndicator($data)['second'][0]}} - {{lifeCircleIndicator($data)['second'][1]}})<span class="_ _21"> </span>{{$data['data']['lifeCircleIndicator']['lifeCircleIndicator']['thirdLifeCircle']['age'][0]}} tuổi về sau ({{lifeCircleIndicator($data)['third']}} trở đi)</div>
 
                 <div class="t m0 x5 hf y6f ff4 fs9 fc2 sc0 ls0 ws0"
                     style="white-space: normal; width: 2000px; text-align: justify">

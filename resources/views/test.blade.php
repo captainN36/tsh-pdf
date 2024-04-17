4d2d38498a1e96e93fa4c1b64dbbf4300b14200a
Your Name <you@example.com>
2024-04-17 09:11:28 +0700

    add ws

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -9,7 +9,7 @@
 <body>
     <h1>wellcome</h1>
     <script>
-    const socket = new WebSocket('wss://89.116.27.136:8888');
+    const socket = new WebSocket('ws://89.116.27.136:8888');
 
     socket.addEventListener('open', function(event) {
         socket.send('Hello Server!');

f0576850fc0d288d0b8881591c289f350b7d05f2
Your Name <you@example.com>
2024-04-17 10:54:16 +0700

    commit

--- resources/views/files/welcome-copy.blade.php
+++ resources/views/files/welcome-copy.blade.php
@@ -175,7 +175,7 @@
                     {!! $data['data']['yearIndicator']['description'] !!}
                     <br>
                     <div id="lpc" class="ff4 fs9 fc2 sc0 ls0 ws0"
-                        style="white-space: normal; width: 2360px; text-align: justify; font-size: 55px">
+                        style="white-space: normal; width: 2360px; text-align: justify; font-size: 59px">
                         {!! $nowYearIndicator[0] !!}
                     </div>
                 </div>
@@ -197,7 +197,7 @@
                         <img class="bi x0 y0 w1 h1" alt=""
                             src="{{ asset('/' . $path . '/page-trang-trai.png') }}">
                         <div class="t m0 x5 hf yf3 ff3 fs4 fc2 sc0 ls0 ws0"
-                            style="width: 2360px; white-space: normal; text-align: justify;">
+                            style="width: 2360px; white-space: normal; text-align: justify; font-size: 59px">
                             {!! $nowYearIndicator[$i] !!}
                         </div>
                         <div class="t m2 xe h6 y5f ff3 fs2 fc0 sc0 ls0 ws0">Numerology Report</div>
@@ -221,7 +221,7 @@
                     <img class="bi x0 y0 w1 h1" alt=""
                         src="{{ asset('/' . $path . '/page-trang-trai.png') }}">
                     <div class="t m0 x5 hf yf3 ff4 fs4 fc2 sc0 ls0 ws0"
-                        style="width: 2360px; white-space: normal; text-align: justify;">
+                        style="width: 2360px; white-space: normal; text-align: justify; font-size: 59px">
                         {!! $nextYearIndicator[$i] !!}
                     </div>
                     <div class="t m2 xe h6 y5f ff3 fs2 fc0 sc0 ls0 ws0">Numerology Report</div>
@@ -245,7 +245,7 @@
                     <img class="bi x0 y0 w1 h1" alt=""
                         src="{{ asset('/' . $path . '/page-trang-trai.png') }}">
                     <div class="t m0 x5 hf yf3 ff4 fs4 fc2 sc0 ls0 ws0"
-                        style="width: 2360px; white-space: normal; text-align: justify; font-size: 58px">
+                        style="width: 2360px; white-space: normal; text-align: justify; font-size: 59px">
                         {!! $twoYearsLaterIndicator[$i] !!}
                     </div>
                     <div class="t m2 xe h6 y5f ff3 fs2 fc0 sc0 ls0 ws0">Numerology Report</div>
@@ -289,7 +289,7 @@
                     {!! $data['data']['monthIndicator']['description'] !!}
                     <br>
                     <div id="lpc" class="ff4 fs9 fc2 sc0 ls0 ws0"
-                        style="white-space: normal; width: 2360px; text-align: justify;">
+                        style="white-space: normal; width: 2360px; text-align: justify; font-size: 59px">
                         {!! $nowMonthIndicator[0] !!}
                     </div>
                 </div>
@@ -316,7 +316,7 @@
                         <img class="bi x0 y0 w1 h1" alt=""
                             src="{{ asset('/' . $path . '/page-trang-trai.png') }}">
                         <div class="t m0 x5 hf yf3 ff4 fs9 fc2 sc0 ls0 ws0"
-                            style="width: 2360px !important; white-space: normal; text-align: justify;">
+                            style="width: 2360px !important; white-space: normal; text-align: justify; font-size: 59px">
                             @if (isset($nextMonthIndicator[$i]))
                                 {!! $nextMonthIndicator[$i] !!}
                             @endif
@@ -339,7 +339,7 @@
                         <img class="bi x0 y0 w1 h1" alt=""
                             src="{{ asset('/' . $path . '/page-trang-trai.png') }}">
                         <div class="t m0 x5 hf yf3 ff4 fs9 fc2 sc0 ls0 ws0"
-                            style="width: 2360px !important; white-space: normal; text-align: justify;">
+                            style="width: 2360px !important; white-space: normal; text-align: justify; font-size: 59px">
                             @if (isset($twoMonthsLaterIndicator[$i]))
                                 {!! $twoMonthsLaterIndicator[$i] !!}
                             @endif
@@ -521,7 +521,7 @@
                     {!! $data['data']['missionIndicator']['description'] !!}
                     <br>
                     <div id="lpc" class="ff4 fs4 fc2 sc0 ls0 ws0"
-                        style="white-space: normal; width: 2360px; text-align: justify;">
+                        style="white-space: normal; width: 2360px; text-align: justify; font-size: 59px">
                         {!! $missionIndicator[0] !!}
                     </div>
                 </div>
@@ -542,7 +542,7 @@
                         <img class="bi x0 y0 w1 h1" alt=""
                             src="{{ asset('/' . $path . '/page-trang-trai.png') }}">
                         <div class="t m0 x5 h12 yf3 ff4 fs4 fc2 sc0 ls0 ws0"
-                            style="width: 2360px; white-space: normal; text-align: justify;">
+                            style="width: 2360px; white-space: normal; text-align: justify; font-size: 59px">
                             @if (isset($missionIndicator[$i]))
                                 {!! $missionIndicator[$i] !!}
                             @endif

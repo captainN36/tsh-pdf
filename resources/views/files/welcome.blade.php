83a6aac2754b8447bc525de5358c766ffe5d0e1c
Your Name <you@example.com>
2024-04-19 12:47:34 +0700

    commit

--- resources/views/files/welcome.blade.php
+++ resources/views/files/welcome.blade.php
@@ -175,7 +175,7 @@
                     {!! $data['data']['yearIndicator']['description'] !!}
                     <br>
                     <div id="lpc" class="ff4 fs9 fc2 sc0 ls0 ws0"
-                        style="white-space: normal; width: 2360px; text-align: justify; font-size: 55px">
+                        style="white-space: normal; width: 2360px; text-align: justify; font-size: 56px">
                         {!! $nowYearIndicator[0] !!}
                     </div>
                 </div>

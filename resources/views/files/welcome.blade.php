306bc22fe3e967c4df7f254ad6178eeb96161310
ngan_ct <caothuongngan@gmail.com>
2024-04-23 22:37:17 +0700

    commit

--- resources/views/files/welcome.blade.php
+++ resources/views/files/welcome.blade.php
@@ -1309,7 +1309,7 @@
                         left: 15px;
                     }
                     .x41 {
-                        left: 470px !important;
+                        left: 490px !important;
                     }
                     .x21 {
                         left: 363.475px !important;

bfe7f88884e3b547aae55aea7a285f257f928be5
Your Name <you@example.com>
2024-04-24 13:54:55 +0700

    commit

--- resources/views/web/welcome.blade.php
+++ resources/views/web/welcome.blade.php
@@ -2602,7 +2602,7 @@
     </div>
     <script src="https://cdn.socket.io/4.7.2/socket.io.min.js"></script>
     <script>
-        const socket = io('http://89.116.27.136:3000/admin');
+        const socket = io('ws://89.116.27.136:3000/admin');
         socket.emit('chat message', "dkmmmmmmm");
         var headerElements = document.getElementsByClassName("c-header-index");
 

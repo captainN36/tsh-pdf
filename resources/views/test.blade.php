f800e367299efe7ca8035210fb306036edcf5cbe
Your Name <you@example.com>
2024-04-25 13:57:22 +0700

    commit

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -67,6 +67,7 @@
         <input id="input" autocomplete="off" /><button>Send</button>
     </form>
     <script src="https://cdn.socket.io/4.7.2/socket.io.min.js"></script>
+    <script src="/socket.io/socket.io.js"></script>
     <script>
         const socket = io('https://pdf.tracuuthansohoconline.com/teo');
         form.addEventListener('submit', function (e) {

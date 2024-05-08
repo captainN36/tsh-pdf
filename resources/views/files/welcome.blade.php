13b958ffaeb26798ee7c40449948eb1bf1cbbcc5
ngan_ct <caothuongngan@gmail.com>
2024-05-08 23:05:44 +0700

    edit font-size

--- resources/views/files/welcome.blade.php
+++ resources/views/files/welcome.blade.php
@@ -370,7 +370,7 @@
                 $pth = $path == 9 ? 8 : $path;
                 ?>
                 <img class="bi x0 y0 w1 h1" alt="" src="{{ asset('/' . $pth . '/bannga.png') }}">
-                <div class="t m0 x13 hd y93 ff1 fs7 fc0 sc0 ls0 ws0" style="left: 92px">4</div>
+                <div class="t m0 x13 hd y93 ff1 fs7 fc0 sc0 ls0 ws0" style="left: 110px; bottom: 1440px">4</div>
                 <div class="t m0 x14 he y94 ff1 fs8 fc0 sc0 ls0 ws0">NHÓM TÍNH CÁCH THEO BẢN NGÃ</div>
 
                 <div class="t m0 hf ff2 fs9 fc2 sc0 ls0 ws0"

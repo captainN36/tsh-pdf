8349a4c9834177cb9515b5e12ee7df35e8adc963
Your Name <you@example.com>
2024-05-14 10:42:12 +0700

    commit

--- resources/views/web/welcome.blade.php
+++ resources/views/web/welcome.blade.php
@@ -2424,7 +2424,7 @@
             <div class="pc pc6 w0 h0 opened">
                 <img class="bi x0 y0 w1 h1" alt="" src="{{ asset('/' . $path . '/vochong.png') }}">
                 <div class="t m0 x13 hd y93 ff1 fs7 fc0 sc0 ls0 ws0 c-header-index">26</div>
-                <div id="report" class="t m0 x14 he y94 ff1 fs8 fc0 sc0 ls0 ws0">TỔNG QUAN DỮ LIỆU BÁO CÁO</div>
+                <div id="report" class="t m0 x14 he y94 ff4 fs8 fc0 sc0 ls0 ws0" style="font-weight: bold">TỔNG QUAN DỮ LIỆU BÁO CÁO</div>
                 <div class="t m0 x5 hf ff2 fs9 fc2 sc0 ls0 ws0" style="left: 0; top: 70px">
                     <img src="{{ asset('/' . $path . '/report.png') }}" alt="" width="2000px">
                 </div>

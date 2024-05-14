53fe4ecf351a94e14808757a46c0d7d1f8dca327
Your Name <you@example.com>
2024-05-14 09:56:46 +0700

    commit

--- resources/views/web/welcome.blade.php
+++ resources/views/web/welcome.blade.php
@@ -140,10 +140,104 @@
             </div>
         </div>
 
-        <div id="pfb" class="pf w0 h0" data-page-no="8">
+        
+
+
+        <div id="pfd" class="pf w0 h0" data-page-no="12">
             <div class="pc pcb w0 h0 opened">
                 <img class="bi x0 y0 w1 h1" alt="" src="{{ asset('/' . $path . '/chi-so-nam.png') }}">
                 <div class="t m0 x13 hd y93 ff1 fs7 fc0 sc0 ls0 ws0" style="left: 92px">2</div>
+                <div class="t m0 x14 he y94 ff1 fs8 fc0 sc0 ls0 ws0">Chỉ số các tháng</div>
+                <div class="t m0 x4d h12 y64 ff3 fs4 fc7 sc0 ls0 ws0">Tháng {{ now()->format('m/Y') }}</div>
+                <div class="t m0 x4e h14 y1c7 ff1 fsc fc7 sc0 ls0 ws0" style="left: 485px; bottom: 1120px">
+                    {{ $data['data']['monthIndicator']['nowMonthIndicator']['monthIndicator']['nowMonthIndicator'] }}
+                </div>
+                <div class="t m0 x4d h12 y66 ff3 fs4 fc7 sc0 ls0 ws0">Tháng {{ now()->addMonths(1)->format('m/Y') }}
+                </div>
+                <div class="t m0 x4e h14 y1c8 ff1 fsc fc7 sc0 ls0 ws0" style="left: 485px; bottom: 1060px">
+                    {{ $data['data']['monthIndicator']['nowMonthIndicator']['monthIndicator']['nextMonthIndicator'] }}
+                </div>
+                <div class="t m0 x4d h12 y68 ff3 fs4 fc7 sc0 ls0 ws0">Tháng {{ now()->addMonths(2)->format('m/Y') }}
+                </div>
+
+                <div class="t m0 x4e h14 y1c9 ff1 fsc fc7 sc0 ls0 ws0" style="left: 485px; bottom: 1000px">
+                    {{ $data['data']['monthIndicator']['nowMonthIndicator']['monthIndicator']['twoMonthsLaterIndicator'] }}
+                </div>
+
+                <?php
+                $nowMonthIndicator = contentText($data['data']['monthIndicator']['firstContent']);
+                ?>
+                <div class="t m0 x5 hf yd7 ff2 fs4 fc2 sc0 ls0 ws0"
+                    style="white-space: normal; width: 2000px; bottom: 900px; text-align: justify;">
+                    {!! $data['data']['monthIndicator']['description'] !!}
+                    <br>
+                    <div id="lpc" class="ff4 fs9 fc2 sc0 ls0 ws0"
+                        style="white-space: normal; width: 2000px; text-align: justify;">
+                        {!! $nowMonthIndicator[0] !!}
+                    </div>
+                </div>
+
+                <div class="t m2 xa h6 y5f ff3 fs2 fc0 sc0 ls0 ws0">Numerology Report</div>
+                @include('footer', ['name' => $data['fullName'], 'date' => $data['dateOfBirth']])
+                <div class="t m0 x3a h5 y61 ff2 fs2 fc0 sc0 ls0 ws0"><?php echo $page; ?></div>
+            </div>
+            <div class="pi" data-data="{&quot;ctm&quot;:[1.500000,0.000000,0.000000,1.500000,0.000000,0.000000]}">
+            </div>
+        </div>
+
+        <?php
+        $nextMonthIndicator = contentText($data['data']['monthIndicator']['nextMonthIndicator']['content']);
+        $twoMonthsLaterIndicator = contentText($data['data']['monthIndicator']['twoMonthsLaterIndicator']['content']);
+
+        ?>
+        @for ($i = 0; $i < count($nextMonthIndicator); $i++)
+            <?php $page++; ?>
+            <div id="pfd" class="pf w0 h0" data-page-no="13">
+                <div class="pc pcb w0 h0 opened">
+                    <img class="bi x0 y0 w1 h1" alt=""
+                        src="{{ asset('/' . $path . '/page-trang-trai.png') }}">
+                    <div class="t m0 x5 hf yf3 ff4 fs9 fc2 sc0 ls0 ws0"
+                        style="width: 2000px; white-space: normal; text-align: justify;">
+                        @if (isset($nextMonthIndicator[$i]))
+                            {!! $nextMonthIndicator[$i] !!}
+                        @endif
+                    </div>
+
+                    <div class="t m2 xe h6 y5f ff3 fs2 fc0 sc0 ls0 ws0">Numerology Report</div>
+                    @include('footer', ['name' => $data['fullName'], 'date' => $data['dateOfBirth']])
+                    <div class="t m0 x3b h5 y61 ff2 fs2 fc0 sc0 ls0 ws0"><?php echo $page; ?></div>
+                </div>
+                <div class="pi"
+                    data-data="{&quot;ctm&quot;:[1.500000,0.000000,0.000000,1.500000,0.000000,0.000000]}"></div>
+            </div>
+        @endfor
+
+        @for ($i = 0; $i < count($twoMonthsLaterIndicator); $i++)
+            <?php $page++; ?>
+            <div id="pfd" class="pf w0 h0" data-page-no="13">
+                <div class="pc pcb w0 h0 opened">
+                    <img class="bi x0 y0 w1 h1" alt=""
+                        src="{{ asset('/' . $path . '/page-trang-trai.png') }}">
+                    <div class="t m0 x5 hf yf3 ff4 fs9 fc2 sc0 ls0 ws0"
+                        style="width: 2000px; white-space: normal; text-align: justify;">
+                        @if (isset($twoMonthsLaterIndicator[$i]))
+                            {!! $twoMonthsLaterIndicator[$i] !!}
+                        @endif
+                    </div>
+
+                    <div class="t m2 xe h6 y5f ff3 fs2 fc0 sc0 ls0 ws0">Numerology Report</div>
+                    @include('footer', ['name' => $data['fullName'], 'date' => $data['dateOfBirth']])
+                    <div class="t m0 x3b h5 y61 ff2 fs2 fc0 sc0 ls0 ws0"><?php echo $page; ?></div>
+                </div>
+                <div class="pi"
+                    data-data="{&quot;ctm&quot;:[1.500000,0.000000,0.000000,1.500000,0.000000,0.000000]}"></div>
+            </div>
+        @endfor
+
+        <div id="pfb" class="pf w0 h0" data-page-no="8">
+            <div class="pc pcb w0 h0 opened">
+                <img class="bi x0 y0 w1 h1" alt="" src="{{ asset('/' . $path . '/chi-so-nam.png') }}">
+                <div class="t m0 x13 hd y93 ff1 fs7 fc0 sc0 ls0 ws0" style="left: 92px">3</div>
                 <div class="t m0 x14 he y94 ff1 fs8 fc0 sc0 ls0 ws0">CHỈ SỐ CÁC NĂM</div>
                 <div class="t m0 x4d h12 y64 ff3 fs4 fc7 sc0 ls0 ws0">NĂM {{ now()->year }}</div>
                 <div class="t m0 x4e h14 y1c7 ff1 fsc fc7 sc0 ls0 ws0" style="left: 485px; bottom: 1120px">
@@ -281,98 +375,6 @@
             </div>
         @endfor
 
-
-
-        <div id="pfd" class="pf w0 h0" data-page-no="12">
-            <div class="pc pcb w0 h0 opened">
-                <img class="bi x0 y0 w1 h1" alt="" src="{{ asset('/' . $path . '/chi-so-nam.png') }}">
-                <div class="t m0 x13 hd y93 ff1 fs7 fc0 sc0 ls0 ws0" style="left: 92px">3</div>
-                <div class="t m0 x14 he y94 ff1 fs8 fc0 sc0 ls0 ws0">Chỉ số các tháng</div>
-                <div class="t m0 x4d h12 y64 ff3 fs4 fc7 sc0 ls0 ws0">Tháng {{ now()->format('m/Y') }}</div>
-                <div class="t m0 x4e h14 y1c7 ff1 fsc fc7 sc0 ls0 ws0" style="left: 485px; bottom: 1120px">
-                    {{ $data['data']['monthIndicator']['nowMonthIndicator']['monthIndicator']['nowMonthIndicator'] }}
-                </div>
-                <div class="t m0 x4d h12 y66 ff3 fs4 fc7 sc0 ls0 ws0">Tháng {{ now()->addMonths(1)->format('m/Y') }}
-                </div>
-                <div class="t m0 x4e h14 y1c8 ff1 fsc fc7 sc0 ls0 ws0" style="left: 485px; bottom: 1060px">
-                    {{ $data['data']['monthIndicator']['nowMonthIndicator']['monthIndicator']['nextMonthIndicator'] }}
-                </div>
-                <div class="t m0 x4d h12 y68 ff3 fs4 fc7 sc0 ls0 ws0">Tháng {{ now()->addMonths(2)->format('m/Y') }}
-                </div>
-
-                <div class="t m0 x4e h14 y1c9 ff1 fsc fc7 sc0 ls0 ws0" style="left: 485px; bottom: 1000px">
-                    {{ $data['data']['monthIndicator']['nowMonthIndicator']['monthIndicator']['twoMonthsLaterIndicator'] }}
-                </div>
-
-                <?php
-                $nowMonthIndicator = contentText($data['data']['monthIndicator']['firstContent']);
-                ?>
-                <div class="t m0 x5 hf yd7 ff2 fs4 fc2 sc0 ls0 ws0"
-                    style="white-space: normal; width: 2000px; bottom: 900px; text-align: justify;">
-                    {!! $data['data']['monthIndicator']['description'] !!}
-                    <br>
-                    <div id="lpc" class="ff4 fs9 fc2 sc0 ls0 ws0"
-                        style="white-space: normal; width: 2000px; text-align: justify;">
-                        {!! $nowMonthIndicator[0] !!}
-                    </div>
-                </div>
-
-                <div class="t m2 xa h6 y5f ff3 fs2 fc0 sc0 ls0 ws0">Numerology Report</div>
-                @include('footer', ['name' => $data['fullName'], 'date' => $data['dateOfBirth']])
-                <div class="t m0 x3a h5 y61 ff2 fs2 fc0 sc0 ls0 ws0"><?php echo $page; ?></div>
-            </div>
-            <div class="pi" data-data="{&quot;ctm&quot;:[1.500000,0.000000,0.000000,1.500000,0.000000,0.000000]}">
-            </div>
-        </div>
-
-        <?php
-        $nextMonthIndicator = contentText($data['data']['monthIndicator']['nextMonthIndicator']['content']);
-        $twoMonthsLaterIndicator = contentText($data['data']['monthIndicator']['twoMonthsLaterIndicator']['content']);
-
-        ?>
-        @for ($i = 0; $i < count($nextMonthIndicator); $i++)
-            <?php $page++; ?>
-            <div id="pfd" class="pf w0 h0" data-page-no="13">
-                <div class="pc pcb w0 h0 opened">
-                    <img class="bi x0 y0 w1 h1" alt=""
-                        src="{{ asset('/' . $path . '/page-trang-trai.png') }}">
-                    <div class="t m0 x5 hf yf3 ff4 fs9 fc2 sc0 ls0 ws0"
-                        style="width: 2000px; white-space: normal; text-align: justify;">
-                        @if (isset($nextMonthIndicator[$i]))
-                            {!! $nextMonthIndicator[$i] !!}
-                        @endif
-                    </div>
-
-                    <div class="t m2 xe h6 y5f ff3 fs2 fc0 sc0 ls0 ws0">Numerology Report</div>
-                    @include('footer', ['name' => $data['fullName'], 'date' => $data['dateOfBirth']])
-                    <div class="t m0 x3b h5 y61 ff2 fs2 fc0 sc0 ls0 ws0"><?php echo $page; ?></div>
-                </div>
-                <div class="pi"
-                    data-data="{&quot;ctm&quot;:[1.500000,0.000000,0.000000,1.500000,0.000000,0.000000]}"></div>
-            </div>
-        @endfor
-
-        @for ($i = 0; $i < count($twoMonthsLaterIndicator); $i++)
-            <?php $page++; ?>
-            <div id="pfd" class="pf w0 h0" data-page-no="13">
-                <div class="pc pcb w0 h0 opened">
-                    <img class="bi x0 y0 w1 h1" alt=""
-                        src="{{ asset('/' . $path . '/page-trang-trai.png') }}">
-                    <div class="t m0 x5 hf yf3 ff4 fs9 fc2 sc0 ls0 ws0"
-                        style="width: 2000px; white-space: normal; text-align: justify;">
-                        @if (isset($twoMonthsLaterIndicator[$i]))
-                            {!! $twoMonthsLaterIndicator[$i] !!}
-                        @endif
-                    </div>
-
-                    <div class="t m2 xe h6 y5f ff3 fs2 fc0 sc0 ls0 ws0">Numerology Report</div>
-                    @include('footer', ['name' => $data['fullName'], 'date' => $data['dateOfBirth']])
-                    <div class="t m0 x3b h5 y61 ff2 fs2 fc0 sc0 ls0 ws0"><?php echo $page; ?></div>
-                </div>
-                <div class="pi"
-                    data-data="{&quot;ctm&quot;:[1.500000,0.000000,0.000000,1.500000,0.000000,0.000000]}"></div>
-            </div>
-        @endfor
 
         <?php
         $array = [];

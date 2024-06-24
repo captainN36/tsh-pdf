bff58f996bed9a625acd0b700ca5e6a9443eac2f
Your Name <you@example.com>
2024-06-24 16:05:16 +0700

    commit

--- resources/views/web/welcome.blade.php
+++ resources/views/web/welcome.blade.php
@@ -2470,7 +2470,7 @@
 
         <div id="pf7" class="pf w0 h0" data-page-no="54">
             <div class="pc pc6 w0 h0 opened">
-                <img class="bi x0 y0 w1 h1" alt="" src="{{ asset('/' . $path . '/vochong.png') }}">
+                <img class="bi x0 y0 w1 h1" style="width: 914px" alt="" src="{{ asset('/' . $path . '/vochong.png') }}">
                 <div class="t m0 x13 hd y93 ff1 fs7 fc0 sc0 ls0 ws0 c-header-index">26</div>
                 <div id="report" class="t m0 x14 he y94 ff4 fs8 fc0 sc0 ls0 ws0" style="font-weight: bold">TỔNG QUAN DỮ LIỆU BÁO CÁO</div>
                 <div class="t m0 x5 hf ff2 fs9 fc2 sc0 ls0 ws0" style="left: 0; top: 70px">
@@ -2650,9 +2650,9 @@
                         </table>
                     </div>
                 </div>
-                <div class="t m2 xa h6 y5f ff3 fs2 fc0 sc0 ls0 ws0">Numerology Report</div>
+                <div class="t m2 xa h6 y5f ff3 fs2 fc0 sc0 ls0 ws0" style="left: 868px">Numerology Report</div>
                 @include('footer')
-                <div class="t m0 x3a h5 y61 ff2 fs2 fc0 sc0 ls0 ws0">{{ $page + 1 }}</div>
+                <div style="left: 817px" class="t m0 x3a h5 y61 ff2 fs2 fc0 sc0 ls0 ws0">{{ $page + 1 }}</div>
                 <div class="pi"
                     data-data="{&quot;ctm&quot;:[1.500000,0.000000,0.000000,1.500000,0.000000,0.000000]}">
                 </div>

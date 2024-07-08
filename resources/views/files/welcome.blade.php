2f9d5f3f07e128e8ad9f19fb5e1d5be2ba48bff8
Your Name <you@example.com>
2024-07-08 10:36:35 +0700

    commit

--- resources/views/files/welcome.blade.php
+++ resources/views/files/welcome.blade.php
@@ -90,6 +90,10 @@
                         white-space: break-spaces;
                         font-size: 45px;
                     }
+
+                    p {
+                        margin-top: 15px;
+                    }
                 </style>
                 <div class="t m0 hf yaf ff2 fs9 fc2 sc0 ls0 ws0"
                     style="flex-wrap: wrap; left: 90px; bottom: 1125px; width: 1950px; text-aligin: justify">

0fcd0a1ebc8066e065ecc5a0fa7a860cb746f009
Your Name <you@example.com>
2024-07-11 10:41:18 +0700

    commit

--- resources/views/web/welcome-copy.blade.php
+++ resources/views/web/welcome-copy.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,6 +10,7 @@
         html {
             line-height: 1.5rem;
         }
+
         /* Đặt kích thước trang theo A4 */
         .pdf-page {
             width: 210mm;
@@ -43,23 +45,73 @@
                 border: none;
                 page-break-after: always;
             }
-        }
 
+            p {
+                margin: 0;
+            }
+
+            html {
+                line-height: 1.5rem;
+            }
+        }
     </style>
 </head>
+
 <body>
     <div class="pdf-page">
-        <div class="" style="
+        <div class=""
+            style="
     padding-left: 62px;
     padding-right: 62px;
     padding-bottom: 115px;
     font-size: 17px;
     margin-top: 83px;
-"><div class=""><div class="mt-[110px] text-[15px] italic"></div></div><div class="mt-6" style="
+">
+            <div class="">
+                <div class="mt-[110px] text-[15px] italic"></div>
+            </div>
+            <div class="mt-6" style="
     margin-top: 1.5rem;
     text-align: justify;
-"><p style="
-">Những con số này cho biết ở mỗi năm bạn nên tập trung định hướng phát triển theo con số nào. Thường thì cuộc đời sẽ tự đẩy bạn đi theo những con số này. Nếu đi lệch, bạn thường sẽ bị cảm thấy cuộc sống mất cân bằng hoặc bất an. Còn nếu đi đúng hướng bạn thường cảm thấy rất bình an và thuận lợi.  Lưu ý: Sau khi sử dụng VIP, mỗi năm bạn vào lại website tra cứu hoặc tải lại file để xem luận giải 3 năm tiếp theo và các nội dung luận giải mới nếu có!</p><p>Những con số này cho biết ở mỗi năm bạn nên tập trung định hướng phát triển theo con số nào. Thường thì cuộc đời sẽ tự đẩy bạn đi theo những con số này. Nếu đi lệch, bạn thường sẽ bị cảm thấy cuộc sống mất cân bằng hoặc bất an. Còn nếu đi đúng hướng bạn thường cảm thấy rất bình an và thuận lợi.  Lưu ý: Sau khi sử dụng VIP, mỗi năm bạn vào lại website tra cứu hoặc tải lại file để xem luận giải 3 năm tiếp theo và các nội dung luận giải mới nếu có!</p><p><strong style="color: rgb(0, 0, 0);">VẬN SỐ NĂM 2024 CỦA BẠN LÀ : 5</strong></p><p><strong>CẢM THẤY LỎNG LẺO VÀ TỰ DO, NHIỀU THAY ĐỔI</strong></p><p>Đây sẽ là một năm cho sự thay đổi, phát triển, vui vẻ và tự do. Năm nay có thể có một số sự thay đổi quan trọng về nơi ở, công việc, hướng đi hoặc hoàn cảnh gia đình. Bạn phải tận dụng lợi thế của sự thay đổi đó để giúp bạn tiến lên phía trước. Thách thức của bạn trong năm nay là tiếp tục tập trung vào các mục tiêu dài hạn, cố gắng sắp xếp các dự án quan trọng và xem xét chúng từ đầu đến cuối. Đôi khi bạn có thể cảm thấy bất an nhưng nhìn chung cả năm đều mang đến cơ hội thăng tiến và tiến bộ.</p><p><br></p><p>Về công việc, không giống như năm trước khi mọi thứ có xu hướng ổn định, năm nay dường như có phần lỏng lẻo và có sự thay đổi (có thể về môi trường làm việc, công việc của bạn,...). Đôi khi bạn thấy mình mất hứng thú và trì hoãn các công việc hoặc dự án quan trọng mà bạn cần hoàn thành. Cũng có khi bạn lại sử dụng quá mức các nguồn lực của mình (có thể là sức khỏe, tiền bạc,...). Do đó, lời khuyên là bạn hãy lập kế hoạch cho một mục tiêu nhất định để thăng tiến đồng thời thực hiện các điều chỉnh và quyết định cần thiết để cải tiến. Ngoài ra, với năng lượng của năm số 5, bạn có thể theo đuổi sự nghiệp trong lĩnh vực nghệ thuật biểu diễn như ca hát, diễn xuất hoặc khiêu vũ, hoặc bạn có thể trở thành một nhà văn viết về du lịch, đi khắp thế giới để tìm kiếm những điều thú vị và phiêu lưu mới.</p><p><br></p><p>Trong các mối quan hệ, để có được những điều tốt đẹp nhất trong năm nay, tất cả những thay đổi nên có lợi cho người khác cũng như cho chính bạn. Hãy cố gắng sống hài hòa với mọi người. Bạn có thể sẽ tham gia nhiều cuộc tụ họp xã hội hơn bình Trong các mối quan hệ, để có được những điều tốt đẹp nhất trong năm nay, tất cả những thay đổi nên có lợi cho người khác cũng như cho chính bạn. Hãy cố gắng sống hài hòa với</p></div></div>
+">
+                <p style="
+">Những con số này cho biết ở mỗi năm bạn nên tập trung định hướng phát triển theo con
+                    số nào. Thường thì cuộc đời sẽ tự đẩy bạn đi theo những con số này. Nếu đi lệch, bạn thường sẽ bị
+                    cảm thấy cuộc sống mất cân bằng hoặc bất an. Còn nếu đi đúng hướng bạn thường cảm thấy rất bình an
+                    và thuận lợi. Lưu ý: Sau khi sử dụng VIP, mỗi năm bạn vào lại website tra cứu hoặc tải lại file để
+                    xem luận giải 3 năm tiếp theo và các nội dung luận giải mới nếu có!</p>
+                <p>Những con số này cho biết ở mỗi năm bạn nên tập trung định hướng phát triển theo con số nào. Thường
+                    thì cuộc đời sẽ tự đẩy bạn đi theo những con số này. Nếu đi lệch, bạn thường sẽ bị cảm thấy cuộc
+                    sống mất cân bằng hoặc bất an. Còn nếu đi đúng hướng bạn thường cảm thấy rất bình an và thuận lợi.
+                    Lưu ý: Sau khi sử dụng VIP, mỗi năm bạn vào lại website tra cứu hoặc tải lại file để xem luận giải 3
+                    năm tiếp theo và các nội dung luận giải mới nếu có!</p>
+                <p><strong style="color: rgb(0, 0, 0);">VẬN SỐ NĂM 2024 CỦA BẠN LÀ : 5</strong></p>
+                <p><strong>CẢM THẤY LỎNG LẺO VÀ TỰ DO, NHIỀU THAY ĐỔI</strong></p>
+                <p>Đây sẽ là một năm cho sự thay đổi, phát triển, vui vẻ và tự do. Năm nay có thể có một số sự thay đổi
+                    quan trọng về nơi ở, công việc, hướng đi hoặc hoàn cảnh gia đình. Bạn phải tận dụng lợi thế của sự
+                    thay đổi đó để giúp bạn tiến lên phía trước. Thách thức của bạn trong năm nay là tiếp tục tập trung
+                    vào các mục tiêu dài hạn, cố gắng sắp xếp các dự án quan trọng và xem xét chúng từ đầu đến cuối. Đôi
+                    khi bạn có thể cảm thấy bất an nhưng nhìn chung cả năm đều mang đến cơ hội thăng tiến và tiến bộ.
+                </p>
+                <p><br></p>
+                <p>Về công việc, không giống như năm trước khi mọi thứ có xu hướng ổn định, năm nay dường như có phần
+                    lỏng lẻo và có sự thay đổi (có thể về môi trường làm việc, công việc của bạn,...). Đôi khi bạn thấy
+                    mình mất hứng thú và trì hoãn các công việc hoặc dự án quan trọng mà bạn cần hoàn thành. Cũng có khi
+                    bạn lại sử dụng quá mức các nguồn lực của mình (có thể là sức khỏe, tiền bạc,...). Do đó, lời khuyên
+                    là bạn hãy lập kế hoạch cho một mục tiêu nhất định để thăng tiến đồng thời thực hiện các điều chỉnh
+                    và quyết định cần thiết để cải tiến. Ngoài ra, với năng lượng của năm số 5, bạn có thể theo đuổi sự
+                    nghiệp trong lĩnh vực nghệ thuật biểu diễn như ca hát, diễn xuất hoặc khiêu vũ, hoặc bạn có thể trở
+                    thành một nhà văn viết về du lịch, đi khắp thế giới để tìm kiếm những điều thú vị và phiêu lưu mới.
+                </p>
+                <p><br></p>
+                <p>Trong các mối quan hệ, để có được những điều tốt đẹp nhất trong năm nay, tất cả những thay đổi nên có
+                    lợi cho người khác cũng như cho chính bạn. Hãy cố gắng sống hài hòa với mọi người. Bạn có thể sẽ
+                    tham gia nhiều cuộc tụ họp xã hội hơn bình Trong các mối quan hệ, để có được những điều tốt đẹp nhất
+                    trong năm nay, tất cả những thay đổi nên có lợi cho người khác cũng như cho chính bạn. Hãy cố gắng
+                    sống hài hòa với</p>
+            </div>
+        </div>
     </div>
 </body>
+
 </html>

1b63f741b59011385ac7f9e8b04b1557582adf24
Your Name <you@example.com>
2024-07-11 10:52:59 +0700

    commit

--- resources/views/web/welcome-copy.blade.php
+++ resources/views/web/welcome-copy.blade.php
@@ -64,7 +64,7 @@
                 padding-left: 62px;
                 padding-right: 62px;
                 padding-bottom: 115px;
-                font-size: 20px;
+                font-size: 25px;
                 margin-top: 83px;
             ">
             <div class="">

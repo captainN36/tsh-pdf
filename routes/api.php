a17c8ca4aaef19f45c5a29e0e11d72069f12815c
Hoàng Huy <53051372+MyScouts@users.noreply.github.com>
2024-09-16 16:11:42 +0700

    make test controller

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PDFController;
+use App\Http\Controllers\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,5 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('render', [PDFController::class, 'renderPDF']);
+Route::post('render', [TestController::class, 'index']);
 // Route::post('data', [PDFController::class, 'getData']);

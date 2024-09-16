d88cfa434614ad2b8009de845035d87f799ecf99
Hoàng Huy <53051372+MyScouts@users.noreply.github.com>
2024-09-16 15:55:21 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/view-file', [PDFController::class, 'view']);
-Route::get('/new-file', [PDFController::class, 'niewFile']);
-Route::get('/a', [PDFController::class, 'pdf']);
-Route::get('/download-file', [PDFController::class, 'downLoad']);
+// Route::get('/view-file', [PDFController::class, 'view']);
+// Route::get('/new-file', [PDFController::class, 'niewFile']);
+// Route::get('/a', [PDFController::class, 'pdf']);
+// Route::get('/download-file', [PDFController::class, 'downLoad']);
 
-Route::get('/html', [PDFController::class, 'mappingKey']);
-Route::get('/', [PDFController::class, 'index']);
-Route::post('/render', [PDFController::class, 'renderViewData']);
-Route::get('/test', [PDFController::class, 'test']);
-Route::get('/test2', [PDFController::class, 'test2'])->name('test2');
+// Route::get('/html', [PDFController::class, 'mappingKey']);
+// Route::get('/', [PDFController::class, 'index']);
+// Route::post('/render', [PDFController::class, 'renderViewData']);
+// Route::get('/test', [PDFController::class, 'test']);
+// Route::get('/test2', [PDFController::class, 'test2'])->name('test2');

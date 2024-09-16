2b4a6e2fc1b4a8642d3d969c64119cf78a412fcb
Hoàng Huy <53051372+MyScouts@users.noreply.github.com>
2024-09-16 15:56:37 +0700

    update route

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::post('render', [PDFController::class, 'renderPDF']);
+Route::post('render', [PDFController::class, 'renderPDF']);
 // Route::post('data', [PDFController::class, 'getData']);
